d451b5e6e8d9846722271436bb2865c169f078ed
Julia Mason <juliagmason@gmail.com>
2022-03-21 16:19:10 -0400

    Calculate RDAs met by species and total catch; will probably just keep the species level code. Did some digging as to why the values are very different for RDAs vs. nutricast demand met. It's partly from looking at split population groups (children, men, women) and partly because Chris used an optimization function based on EARs (which mean 50% are well nourished) to get to 95% well nourished, but this calls for more than the RDAs do.

--- Scripts/Calculate_RDAs_met.R
+++ Scripts/Calculate_RDAs_met.R
@@ -5,15 +5,22 @@
 # following hicks et al more closely
 library (tidyverse)
 
-ds_spp_nutr_content <- readRDS("Data/ds_spp_nutr_content_FishNutrientsGENuS_RDA_groups.Rds")
 
-# Species specific projection data from Chris Free 2020 ----
+# RDAs data ----
+# made in Species_level_nutrient_content
+rda_groups <- readRDS("Data/RDAs_5groups.Rds")
 
-ds_spp <- readRDS("Data/Free_etal_2020_country_level_outcomes_time_series_for_julia.Rds")
+
+# baseline catch data in servings ----
+ds_catch_nutr_yield_baseline <- readRDS("Data/ds_catch_nutr_yield_baseline.Rds")
+
+# projected catch data with nutrients in servings ----
+# from Free data. Convert_catch_to_nutrients.R
+catch_nutr_servings <- readRDS("Data/ds_catch_nutr_yield_servings.Rds")
 
 # population data ----
 
-# downloaded from https://population.un.org/wpp/Download/Standard/CSV/
+# downloaded from https://population.un.org/wpp/Download/Standard/CSV/ on 3/17/2022
 # Population by 5-year age groups.
 # PopMale: Male population in the age group (thousands)
 # PopFemale: Female population in the age group (thousands)
@@ -21,7 +28,7 @@ ds_spp <- readRDS("Data/Free_etal_2020_country_level_outcomes_time_series_for_ju
 
 pop <- read_csv("Data/WPP2019_PopulationByAgeSex_Medium.csv")
 
-# separate into broad adult male/female and child categories
+# separate into broad adult male/female and child categories to  match RDAs
 pop_current <- pop %>%
   filter (Location %in% c("Sierra Leone", "Indonesia", "Peru", "Chile", "Malawi"), Time == 2020) %>%
   mutate (group = 
@@ -33,6 +40,8 @@ pop_current <- pop %>%
   summarize (Pop_Males =  1000 * sum (PopMale[group == "Adult"]),
              Pop_Females = 1000 * sum(PopFemale[group == "Adult"]),
              Pop_Child = 1000 * sum(PopTotal[group == "Child"])) %>%
+             # also have total population that would eat fish--sum of males, females, and children maybe do this later?
+             #Pop_Total = sum (Pop_Males, Pop_Females, Pop_Child)) %>%
   pivot_longer (cols = starts_with("Pop"),
                 names_prefix = "Pop_", 
                 names_to = "group", 
@@ -40,23 +49,35 @@ pop_current <- pop %>%
   rename (country = Location)
   
 
-rdas_met_current_catch <- ds_spp_nutr_content %>%
-  filter (!is.na(amount)) %>%
-  # have to convert catch_mt/yr into 100g servings/day. 1000 kg/mt * 1000 g/kg * 1 serving/100g * 1 yr/365 days
-  mutate (catch_servings = catch_mt * 1000 * 1000 / 100 / 365, 
-          spp_nutr = amount * catch_servings) %>%
-  group_by (country, nutrient, group) %>%
-  mutate (tot_nutr = sum (spp_nutr)) %>%
-  select (country, nutrient, group, unit, mean_rda, tot_nutr) %>%
-  distinct() %>%
+# convert current catch to rdas met ----
+ 
+rdas_met_current_catch <- ds_catch_nutr_yield_baseline %>%
+  # calculate total nutrient servings per day in each country
+  group_by (country, nutrient) %>%
+  summarise (tot_nutr_servings = sum (nutr_servings, na.rm = TRUE)) %>%
+  left_join (rda_groups, by = "nutrient") %>%
   left_join (pop_current, by = c("country", "group")) %>%
-  mutate (yield_per_cap = tot_nutr / population,
+  filter (!is.na(population)) %>%
+  mutate (yield_per_cap = tot_nutr_servings / population,
           rda_needs = mean_rda * population,
-          rda_met = tot_nutr/rda_needs)
+          rda_met = tot_nutr_servings/rda_needs) # this is equivalent to yield_per_cap/mean_rda
 
 
-# really big amounts. sanity check
-  
+# also do this by species ----
+rdas_met_current_catch_spp <- ds_catch_nutr_yield_baseline %>%
+  select (country, species, major_group, nutrient, nutr_servings) %>%
+  left_join (rda_groups, by = "nutrient") %>%
+  left_join (pop_current, by = c("country", "group")) %>%
+  filter (!is.na(population)) %>%
+  mutate (yield_per_cap = nutr_servings / population,
+          rda_needs = mean_rda * population,
+          # proportion of population rda met for each group
+          rda_met = nutr_servings/rda_needs)
+
+saveRDS(rdas_met_current_catch_spp, file = "Data/ds_baseline_RDAs_met.Rds")
+
+
+# plot ----  
 # https://thomasadventure.blog/posts/labels-ggplot2-bar-chart/
 library (ggcharts)
 
@@ -68,3 +89,115 @@ rdas_met_current_catch %>%
   geom_text (aes (label = round(rda_met, 1), y = rda_met + 0.5), position = position_dodge(width = 1)) +
   theme_bw() +
   labs (x = "", fill = "", y = "")
+
+rdas_met_current_catch_spp %>%
+  filter (!is.na(rda_met), species != "Engraulis ringens") %>%
+  ggplot (aes (y = rda_met, x = nutrient, fill = group)) +
+  geom_bar (stat = "identity", position = "dodge") +
+  facet_wrap (~country, nrow = 4, scales = "free") +
+  geom_text (aes (label = round(rda_met, 1), y = rda_met + 0.5), position = position_dodge(width = 1)) +
+  theme_bw() +
+  labs (x = "", fill = "", y = "")
+
+
+# plot for overall population
+rdas_met_current_catch_spp %>%
+  filter (!is.na (nutr_servings)) %>%
+  group_by (country, nutrient, species) %>%
+  mutate (tot_pop = sum(population),
+             tot_rda_needs = sum(rda_needs),
+             tot_nutr_servings = first (nutr_servings),
+             tot_rda_met = nutr_servings / tot_rda_needs) %>%
+  # get rid of groups, otherwise triple counting
+  select (country, species, major_group, nutrient, tot_rda_met) %>%
+  distinct() %>% 
+  ggplot (aes (y = tot_rda_met, x = nutrient, fill = major_group)) +
+  geom_bar (stat = "identity") +
+  facet_wrap (~country, nrow = 4, scales = "free") +
+  theme_bw() +
+  labs (x = "", fill = "", y = "") +
+  facet_wrap (~country, scales = "free") +
+  labs (y = "Proportion of population RDAs met") +
+  ggtitle ("Proportion of population RDAs met by current catch, BAU")
+
+rdas_met_current_catch_spp %>%
+  filter (!is.na (nutr_servings)) %>%
+  group_by (country, nutrient, species) %>%
+  mutate (tot_pop = sum(population),
+          tot_rda_needs = sum(rda_needs),
+          tot_nutr_servings = first (nutr_servings),
+          tot_rda_met = nutr_servings / tot_rda_needs,
+          anchovy = ifelse (species == "Engraulis ringens", "Anchovy", "Other")) %>%
+ ggplot (aes (y = tot_rda_met, x = nutrient, fill = anchovy)) +
+  geom_bar (stat = "identity") +
+  facet_wrap (~country, nrow = 4, scales = "free") +
+  theme_bw() +
+  labs (x = "", fill = "", y = "") +
+  facet_wrap (~country, scales = "free")
+
+# sanity check, why such big amounts?? ----
+
+# nutricast uses nutr_demand with EARs, in mt
+#from nutricast code --> calc_nutr_deficiencies/Step4_calculate_nutrient_demand_hist_proj.R
+# supply required is in metric tons for the whole year
+nutr_demand_free <- readRDS(file.path ("../nutrient_endowment/output/1960_2100_nutrient_demand_by_country.Rds"))
+# CF used _50perc for calculations
+# this is summed for the whole country, with projected population growth (50% estimate; this is where the 50% comes from)
+
+x <- nutr_demand_free %>%
+  filter (country %in% c("Sierra Leone", "Indonesia", "Peru", "Chile"),
+          nutrient %in% c("Calcium", "Iron", "Zinc",  "Vitamin A", "Selenium"),
+          year == 2020) %>%
+  select (country, nutrient, supply_req_mt_yr_50perc)
+
+z <- ds_catch_nutr_yield_baseline %>%
+  group_by (country, nutrient) %>%
+  summarise (yield_mt = sum (nutr_mt, na.rm = TRUE))
+
+# free supply required is considerably bigger than my yield because he used an optimization function based on EARs to achieve 95% of population not nutrient deficient. but based on sd/cv of 0.25, not sure where that comes from; probably should use RDAs. 
+t2 <- rdas_met_current_catch %>%
+  # convert rda needs to mt
+  mutate(
+  dens_units = 
+  case_when (
+    nutrient %in% c("Protein", "Omega_3") ~ "g",
+    nutrient %in% c("Vitamin_A", "Selenium") ~ "ug",
+    TRUE ~ "mg"
+  ),
+  # rda needs are in native unit/day * 365 days/yr * 1kg / 1000 g * 1metric ton / 1000 kg
+  #rda_mt = measurements::conv_unit(rda_needs, dens_units, "metric_ton") * 365
+  rda_mt = case_when (
+    nutrient %in% c ("Calcium", "Iron", "Zinc") ~ rda_needs * 365 / 1000 / 1000 / 1000,
+    nutrient %in% c ("Selenium", "Vitamin_A") ~ rda_needs * 365 / 1000 / 1000 / 1000 / 1000,
+    nutrient %in% c ("Omega_3", "Protein") ~ rda_needs * 365 / 1000 / 1000
+  )
+  ) %>%
+  left_join (z, by = c ("country", "nutrient")) %>%
+  left_join (x, by = c("country", "nutrient"))
+
+
+# problem is unit conversion??
+conv_unit (850 * 2428079, "mg", "metric_ton") * 365
+conv_unit (0.7 * 2428079, "g", "metric_ton") * 365
+0.7 * 2428079 * 365 / 1000 / 1000
+
+# conv_unit not recycling properly when used in nutate; better to do by hand for now
+
+# problem is also that Free method is for the entire population, so I would need to add up all my rda_mt to get there? it's still a lot smaller, but closer to the right order of magnitude. 
+# check RDAs vs Ears
+ears <- readRDS("Data/dietary_reference_intake_data.Rds") %>%
+  # Reduce to EARs
+  filter(dri_type=="Estimated Average Requirement (EAR)" & !is.na(value)) %>%
+  select (nutrient, units, sex_stage, age_range, value) %>%
+  group_by (nutrient, sex_stage) %>%
+  summarise (ear_value = mean(value, na.rm = TRUE)) %>%
+  mutate (group = 
+            case_when (sex_stage == "Women (pregnant)" ~ "Pregnant",
+                       sex_stage == "Women (lactating)" ~ "Lactating", 
+                     sex_stage == "Women" ~ "Females",
+                     sex_stage == "Men" ~ "Males",
+                     sex_stage == "Children" ~ "Child"))
+
+rda_groups %>%
+  left_join (ears, by = c ("nutrient", "group")) %>% View()
+# RDA is consistently larger than EAR; this makes sense. definitely something weird with EARs for protein. 
