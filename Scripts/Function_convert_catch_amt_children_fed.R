7a0d3672a366088415ce0bc5065d656ab665e5f3
Julia Mason <juliagmason@gmail.com>
2022-09-27 15:01:16 -0400

    Standalone function to convert catch in tons to children fed. repeated many times in different ways in other scripts.

---
+++ Scripts/Function_convert_catch_amt_children_fed.R
@@ -0,0 +1,87 @@
+# Function to convert amount of catch to nutrients/children fed
+# 9/27/22
+
+# just want a function where I can plug in a species and an amount and it spits out the children fed for each nutrient?
+
+
+
+library (tidyverse)
+
+# using code from Species_level_nutrient_content.R
+
+# nutrient data ----
+# fish nutrients models, ran code from https://github.com/mamacneil/NutrientFishbase
+# this is per 100g raw, edible portion
+fishnutr <- read_csv ("Data/Species_Nutrient_Predictions.csv")
+
+# truncate to just summary predicted value. eventually will want range?
+fishnutr_mu <- fishnutr %>%
+  select (species, ends_with ("_mu"))
+
+
+fishnutr_long <- fishnutr_mu %>% 
+  pivot_longer (Selenium_mu:Vitamin_A_mu,
+                           names_to = "nutrient",
+                           values_to = "amount") %>%
+  mutate (nutrient = str_sub(nutrient, end = -4))
+
+
+# genus data for nonfish [eventually could use AFCD]
+spp_key <- read.csv(file.path ("../nutrient_endowment/output/Gaines_species_nutrient_content_key.csv"), as.is=T)
+spp_key_long <- spp_key %>% 
+  select (species, major_group, genus_food_name, calcium_mg, iron_mg, polyunsaturated_fatty_acids_g, protein_g, vitamin_a_mcg_rae, zinc_mg) %>%
+ 
+  pivot_longer (calcium_mg:zinc_mg,
+                names_to = "nutrient", 
+                values_to = "amount") %>%
+  # recode major_Group_ name from nutricast code
+mutate (
+  taxa=recode(genus_food_name,
+              "Cephalopod"="Cephalopods",
+              "Crustacean"="Crustaceans",
+              "Demersal Fish"="Finfish",
+              "Marine Fish; Other"="Finfish",
+              "Mollusc; Other"="Molluscs",
+              "Pelagic Fish"="Finfish"),
+  nutrient = recode (nutrient, 
+                    "calcium_mg" = "Calcium",
+                     "iron_mg" = "Iron",
+                     "polyunsaturated_fatty_acids_g" = "Omega_3",
+                     "protein_g" = "Protein", 
+                     "vitamin_a_mcg_rae" = "Vitamin_A", 
+                     "zinc_mg" = "Zinc")
+  
+)
+  
+
+
+rda_groups <- readRDS("Data/RDAs_5groups.Rds")
+rda_child <- rda_groups %>% filter (group == "Child")
+
+calc_children_fed_func <- function (species_name, taxa, amount_mt) {
+  
+  p_edible <- case_when (
+    taxa == "Finfish" ~ 0.87,
+    taxa == "Crustacean" ~ 0.36,
+    taxa == "Mollusc" ~ 0.17,
+    taxa == "Cephalopod" ~ 0.21)
+  
+  if (taxa == "Finfish") {
+    nutr_content <- fishnutr_long %>% filter (species == species_name)
+  } else {
+    nutr_content <- spp_key_long %>% filter (species == species_name)
+  }   
+  
+  catch_nutrients <- nutr_content %>%
+    mutate (catch_mt = amount_mt,
+            # convert tons per year to 100g /day, proportion edible
+            edible_servings = catch_mt * p_edible * 1000 * 1000 /100 / 365,
+            nutrient_servings = edible_servings * amount) %>%
+    left_join (rda_child, by = "nutrient") %>%
+    mutate (children_fed = nutrient_servings / mean_rda)
+  
+
+}
+
+# s <- calc_children_fed_func("Dosidicus gigas", "Cephalopod", 151407)
+# t <- calc_children_fed_func("Trachurus murphyi", "Finfish", 15429)
