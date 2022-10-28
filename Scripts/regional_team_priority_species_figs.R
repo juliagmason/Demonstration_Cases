34e0fa72b17072b248e3024daf99aa53753a4a38
Julia Mason <juliagmason@gmail.com>
2022-10-28 16:32:48 -0400

    nutricast/fishcast time series of all variables to understand model outputs

--- Scripts/regional_team_priority_species_figs.R
+++ Scripts/regional_team_priority_species_figs.R
@@ -567,6 +567,144 @@ mapply (plot_nutricast_proj, ds_pri_spp_input$country, ds_pri_spp_input$species)
 )
 dev.off()
 
+# John also requested biomass, profits
+
+# try to pivot_long so can show on the same graph?
+ds_pri_long <- ds_pri_spp %>%
+  pivot_longer(range_km2:ffmsy, 
+               names_to = "variable",
+               values_to = "value")
+
+ds_pri_long %>%
+  filter (country == "Peru", species == "Mugil cephalus", 
+          !variable %in% c ("catch_mt", "biomass_mt", "profits_usd")) %>%
+  ggplot (aes (x = year, y = value, col = scenario)) +
+  geom_line () +
+  facet_grid (variable ~ rcp, scales = "free_y") +
+  theme_bw() +
+  labs (x = "", y = "") +
+  ggtitle (paste0 ("Free et al. (2020) projections for ", spp_name, ", ", country_name)) +
+  theme (plot.title = element_text (size = 14),
+         axis.text = element_text (size = 12))
+
+
+ds_pri_long %>%
+  filter (country == "Peru", species == "Mugil cephalus", 
+          variable %in% c ("catch_mt", "msy_mt")) %>%
+  ggplot (aes (x = year, y = value, col = variable)) +
+  geom_line () +
+  facet_grid (scenario ~ rcp, scales = "free_y") +
+  theme_bw() +
+  labs (x = "", y = "") +
+  ggtitle (paste0 ("Free et al. (2020) projections for ", spp_name, ", ", country_name)) +
+  theme (plot.title = element_text (size = 14),
+         axis.text = element_text (size = 12))
+
+
+# Plot biomass, catch, profits 
+plot_nutricast_proj<- function (country_name, spp_name) {
+  
+  dat <- ds_pri_long %>% 
+    filter (country == country_name, species == spp_name, year  >2030)
+  
+  dat$scenario <- factor (dat$scenario, levels = c ("No Adaptation", "Productivity Only", "Full Adaptation"))
+  
+  dat$variable <- factor (dat$variable, levels = c ("biomass_mt", "catch_mt", "profits_usd", "range_km2", "msy_mt", "bbmsy", "ffmsy"))
+  
+  p <- dat %>% 
+    filter (variable %in% c ("catch_mt", "biomass_mt", "profits_usd")) %>%
+    ggplot (aes (x = year, y = value, col = scenario)) +
+  geom_line () +
+  facet_grid (variable ~ rcp, scales = "free_y") +
+  theme_bw() +
+  labs (x = "", y = "") +
+  ggtitle (paste0 ("Free et al. (2020) projections for ", spp_name, ", ", country_name)) +
+  theme (plot.title = element_text (size = 14),
+         axis.text = element_text (size = 12))
+
+  
+  print (p)
+  
+}
+
+plot_nutricast_proj ("Sierra Leone", "Sardinella maderensis")
+
+ds_pri_spp_input <- ds_pri_spp %>% filter (!is.na (catch_mt), catch_mt > 0) %>% select (country, species, rank) %>% distinct %>%
+  arrange (country, rank)
+
+pri_input_ls <- list (country_name = ds_pri_spp_input$country, spp_name = ds_pri_spp_input$species)
+
+pdf (file = "Figures/Nutricast_timeseries_Biomass_Catch_Profits.pdf", width = 12, height = 8)
+print(
+  mapply (plot_nutricast_proj, ds_pri_spp_input$country, ds_pri_spp_input$species)
+)
+dev.off()
+
+# Plot other variables
+plot_nutricast_proj<- function (country_name, spp_name) {
+  
+  dat <- ds_pri_long %>% 
+    filter (country == country_name, species == spp_name, year  >2030)
+  
+  dat$scenario <- factor (dat$scenario, levels = c ("No Adaptation", "Productivity Only", "Full Adaptation"))
+  
+  dat$variable <- factor (dat$variable, levels = c ("biomass_mt", "catch_mt", "profits_usd", "range_km2", "msy_mt", "bbmsy", "ffmsy"))
+  
+  p <- dat %>% 
+    filter (!variable %in% c ("catch_mt", "biomass_mt", "profits_usd")) %>%
+    ggplot (aes (x = year, y = value, col = scenario)) +
+    geom_line () +
+    facet_grid (variable ~ rcp, scales = "free_y") +
+    theme_bw() +
+    labs (x = "", y = "") +
+    ggtitle (paste0 ("Free et al. (2020) projections for ", spp_name, ", ", country_name)) +
+    theme (plot.title = element_text (size = 14),
+           axis.text = element_text (size = 12))
+  
+  
+  print (p)
+  
+}
+
+
+pdf (file = "Figures/Nutricast_timeseries_MSY_BBMSY_FFMSY.pdf", width = 12, height = 8)
+print(
+  mapply (plot_nutricast_proj, ds_pri_spp_input$country, ds_pri_spp_input$species)
+)
+dev.off()
+
+# plot catch and msy together
+plot_nutricast_proj<- function (country_name, spp_name) {
+  
+  dat <- ds_pri_long %>% 
+    filter (country == country_name, species == spp_name, year  >2030)
+  
+  dat$scenario <- factor (dat$scenario, levels = c ("No Adaptation", "Productivity Only", "Full Adaptation"))
+  
+  dat$variable <- factor (dat$variable, levels = c ("biomass_mt", "catch_mt", "profits_usd", "range_km2", "msy_mt", "bbmsy", "ffmsy"))
+  
+  p <- dat %>% 
+    filter (variable %in% c ("catch_mt", "msy_mt")) %>%
+    ggplot (aes (x = year, y = value, col = variable)) +
+    geom_line () +
+    facet_grid (scenario ~ rcp, scales = "free_y") +
+    theme_bw() +
+    labs (x = "", y = "") +
+    ggtitle (paste0 ("Free et al. (2020) projections for ", spp_name, ", ", country_name)) +
+    theme (plot.title = element_text (size = 14),
+           axis.text = element_text (size = 12))
+  
+  
+  print (p)
+  
+}
+
+
+pdf (file = "Figures/Nutricast_timeseries_catch_vs_MSY.pdf", width = 12, height = 8)
+print(
+  mapply (plot_nutricast_proj, ds_pri_spp_input$country, ds_pri_spp_input$species)
+)
+dev.off()
 
 # plot micronutrient density vs. % loss nutricast ----
 t <- ds_spp %>%
