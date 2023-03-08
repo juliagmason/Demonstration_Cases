37b1e19bb60bdbe78415d772c56f60c7fc99361c
juliagmason <juliagmason@gmail.com>
2023-03-08 18:12:59 -0500

    quick check compare values with SAU

--- Scripts/Clean_SLE_IHH_landings.R
+++ Scripts/Clean_SLE_IHH_landings.R
@@ -24,6 +24,22 @@ sle_ind <- read_excel("Data/IHH Siera Leone disaggrated small pelagic data.xlsx"
   rename (species = Lowest_taxonomic_id, catch_mt = total_LSF_catch) %>%
   mutate (sector = "Industrial")
 
-sle_landings_ihh <- rbind (sle_ssf, sle_ind)
+sle_landings_ihh <- rbind (sle_ssf, sle_ind) %>%
+  # change species to match SAU/fish nutrients
+  mutate (species = ifelse (species == "Sardinella spp", "Sardinella", species))
 
 saveRDS(sle_landings_ihh, file = "Data/SLE_landings_IHH.Rds")  
+
+
+## compare with sau ----
+sau_2015_2019 <- readRDS("Data/SAU_2015_2019.Rds")
+
+sau_sl <- sau_2015_2019 %>%
+  filter (country == "Sierra Leone", species %in% c ("Ethmalosa fimbriata", "Engraulis encrasicolus", "Sardinella maderensis", "Sardinella aurita", "Sardinella"))
+
+# sau has no industrial catch that is also from sierra leone 
+
+sau_sl %>%
+  filter (fishing_entity == "Sierra Leone") %>%
+  group_by (year, species, fishing_sector) %>%
+  summarise (tonnes = sum (tonnes, na.rm = TRUE)) %>% View()
