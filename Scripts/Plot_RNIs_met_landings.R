54d9e21804aa2c1b4662d1a52eff75682cc677ba
juliagmason <juliagmason@gmail.com>
2023-03-13 14:27:47 -0400

    Reorganized by country. Added a big multitool function to plot comparisons of SAU categories. facet by category, with all nutrients.

--- Scripts/Plot_RNIs_met_landings.R
+++ Scripts/Plot_RNIs_met_landings.R
@@ -18,34 +18,7 @@ priority_spp <- read_csv ("Data/regional_teams_priority_spp.csv") %>%
                                TRUE ~ species)
   )
 
-# Landings data ----
-
-# Chile country specific 
-chl_landings <- readRDS ("Data/Chl_sernapesca_landings_compiled_2012_2021.Rds")
-
-# chile use landings data
-chl_pri_spp_catch <- chl_landings %>%
-  filter (year == 2021) %>%
-  group_by (species) %>%
-  summarise (catch_mt = sum (catch_mt)) %>%
-  right_join (filter (priority_spp, country == "Chile")) %>%
-  mutate (children_fed = pmap (list (species = species, taxa = taxa, amount = catch_mt), calc_children_fed_func)) %>%
-  unnest(cols = c(children_fed),  names_repair = "check_unique") 
-
-# SLE country specific
-sle_landings <- readRDS("Data/SLE_landings_IHH.Rds")
-
-sle_pri_spp_catch <- sle_landings %>%
-  filter (year == 2017) %>%
-  group_by (species) %>%
-  summarise (catch_mt = sum (catch_mt)) %>%
-  mutate (taxa = "Finfish") %>%
-  #right_join (filter (priority_spp, country == "Chile")) %>%
-  mutate (children_fed = pmap (list (species = species, taxa = taxa, amount = catch_mt), calc_children_fed_func)) %>%
-  unnest(cols = c(children_fed),  names_repair = "check_unique") 
-# convert to nutrients
-
-# SAU data ----
+# SAU landings data ----
 
 # as of 10/25/22 just 2019 data, suggested by Deng Palomares. Clipped in SAU_explore.R
 sau_2019 <- readRDS("Data/SAU_2019.Rds") %>%
@@ -56,13 +29,29 @@ sau_2019 <- readRDS("Data/SAU_2019.Rds") %>%
     TRUE ~ species
   ))
 
-
+######################################
 # Chile ----
+
+# country specific landings data ----
+
+#Clean_Chile_Sernapesca_landings.R
+chl_landings <- readRDS ("Data/Chl_sernapesca_landings_compiled_2012_2021.Rds")
+
+# take most recent year and convert to children fed, priority species
+chl_pri_spp_catch <- chl_landings %>%
+  filter (year == 2021) %>%
+  group_by (species) %>%
+  summarise (catch_mt = sum (catch_mt)) %>%
+  right_join (filter (priority_spp, country == "Chile")) %>%
+  mutate (children_fed = pmap (list (species = species, taxa = taxa, amount = catch_mt), calc_children_fed_func)) %>%
+  unnest(cols = c(children_fed),  names_repair = "check_unique") 
+
+# plot 
 png ("Figures/Chile_pri_spp_landings_RNIs_met.png", width = 10, height = 5, units = "in", res = 300)  
 chl_pri_spp_catch %>%
   mutate(
     spp_short = ifelse (
-      species != "Stolephorus",
+      grepl(" ", species),
       paste0 (substr(species, 1, 1), ". ", str_split_fixed (species, " ", 2)[,2]),
       species) 
   ) %>%
@@ -82,13 +71,30 @@ chl_pri_spp_catch %>%
     plot.title = element_text (size = 18))
 dev.off()
 
-# sierra leone IHH ----
+####################################
+# sierra leone ----
+
+# country specific IHH data
+
+#Clean_SLE_IHH_landings.R
+sle_landings <- readRDS("Data/SLE_landings_IHH.Rds")
+
+sle_pri_spp_catch <- sle_landings %>%
+  filter (year == 2017) %>%
+  group_by (species) %>%
+  summarise (catch_mt = sum (catch_mt)) %>%
+  mutate (taxa = "Finfish") %>%
+  # convert to children fed
+  mutate (children_fed = pmap (list (species = species, taxa = taxa, amount = catch_mt), calc_children_fed_func)) %>%
+  unnest(cols = c(children_fed),  names_repair = "check_unique") 
+
+
 png ("Figures/SLE_IHH_pri_spp_landings_RNIs_met.png", width = 10, height = 5, units = "in", res = 300)  
 
 sle_pri_spp_catch %>%
   mutate(
     spp_short = ifelse (
-      species != "Sardinella",
+      grepl(" ", species),
       paste0 (substr(species, 1, 1), ". ", str_split_fixed (species, " ", 2)[,2]),
       species) 
   ) %>%
@@ -108,7 +114,7 @@ sle_pri_spp_catch %>%
     plot.title = element_text (size = 18))
 dev.off()
 
-# split by sector
+# split by sector ind vs artisanal ----
 png ("Figures/SLE_IHH_pri_spp_landings_RNIs_met_sector.png", width = 10, height = 5, units = "in", res = 300) 
 sle_landings %>%
   filter (year == 2017) %>%
@@ -120,7 +126,7 @@ sle_landings %>%
   unnest(cols = c(children_fed),  names_repair = "check_unique")  %>%
   mutate(
     spp_short = ifelse (
-      species != "Sardinella",
+      grepl(" ", species),
       paste0 (substr(species, 1, 1), ". ", str_split_fixed (species, " ", 2)[,2]),
       species) 
   ) %>%
@@ -192,7 +198,7 @@ plot_sau_rnis_met <- function (country_name) {
     unnest(cols = c(children_fed),  names_repair = "check_unique") %>%
     mutate(
       spp_short = ifelse (
-        species != "Stolephorus",
+        grepl(" ", species),
         paste0 (substr(species, 1, 1), ". ", str_split_fixed (species, " ", 2)[,2]),
         species) 
     ) %>%
@@ -234,7 +240,7 @@ print(
 )
 dev.off()
 
-# facet all countries ----
+# facet all SAU countries ----
 
 multicountry_nutr_bank_recent_yr <- sau_2019 %>%
   filter (!country %in% c("Chile", "Mexico")) %>%
@@ -251,7 +257,7 @@ png ("Figures/Facet_pri_spp_RNIs_met.png", width = 12, height = 12, units = "in"
 multicountry_nutr_bank_recent_yr %>%
   mutate(
     spp_short = ifelse (
-      species != "Stolephorus",
+      grepl(" ", species),
       paste0 (substr(species, 1, 1), ". ", str_split_fixed (species, " ", 2)[,2]),
       species) 
   ) %>%
@@ -270,3 +276,111 @@ multicountry_nutr_bank_recent_yr %>%
     legend.title = element_text (size = 14),
     plot.title = element_text (size = 18))
 dev.off()
+
+
+# plot industrial vs. artisanal facet, SAU data ----
+
+# dumb peru anchovy situation
+sau_2015_2019 <- readRDS("Data/SAU_2015_2019.Rds")
+
+# slight tweak, not just looking at domestic, looking overall (value is 0.949 instead of 0.955)
+# only fishmeal and dhc, no "other"
+peru_anchov_dhc <- sau_2015_2019 %>%
+  filter (country == "Peru",  year == 2018, species == "Engraulis ringens") %>%
+  group_by (species) %>%
+  summarise (prop_fishmeal = sum(tonnes[end_use_type ==  "Fishmeal and fish oil"])/sum(tonnes))
+
+peru_anchov_total_2019 <- sau_2015_2019 %>%
+  filter (country == "Peru", year == 2019, species == "Engraulis ringens") %>%
+  pull (tonnes) %>% sum()
+
+# https://stackoverflow.com/questions/24569154/use-variable-names-in-functions-of-dplyr
+
+plot_sau_rnis_met_category <- function (country_name, category, restrict = FALSE) {
+  # categories: fishing_sector, end_use_type, fishing_country
+
+  # remove subsistence and recreational if too cluttered
+  if (category == "fishing_sector") {
+    if (restrict == TRUE) {
+      input_landings <- sau_2019 %>%
+        filter(country == country_name, !fishing_sector %in% c ("Subsistence", "Recreational")) %>%
+        inner_join (priority_spp, by = c ("country", "species")) %>%
+        group_by (species, taxa, !!as.symbol(category)) %>%
+        summarise (catch_mt = sum (tonnes, na.rm = TRUE)) %>%
+        filter (catch_mt > 0)
+    }
+ else { input_landings <- sau_2019 %>%
+      filter (country == country_name) %>%
+      inner_join (priority_spp, by = c ("country", "species")) %>%
+      group_by (species, taxa, !!as.symbol(category)) %>%
+      summarise (catch_mt = sum (tonnes, na.rm = TRUE)) %>%
+      filter (catch_mt > 0)
+  } 
+    }# end fishing_sector if
+  
+  # for Peru end use, have to use dumb hack to fix weird dhc value
+  if (category == "end_use_type") {
+    if (country_name == "Peru") {
+    input_landings <- sau_2019 %>%
+      filter (country == country_name) %>%
+      inner_join (priority_spp, by = c ("country", "species")) %>%
+      group_by (species, taxa, !!as.symbol(category)) %>%
+      summarise (catch_mt = sum (tonnes, na.rm = TRUE)) %>%
+      mutate (catch_mt = case_when(
+        species == "Engraulis ringens" & end_use_type == "Fishmeal and fish oil" ~ peru_anchov_dhc$prop_fishmeal * peru_anchov_total_2019,
+        species == "Engraulis ringens" & end_use_type == "Direct human consumption" ~ (1- peru_anchov_dhc$prop_fishmeal) * peru_anchov_total_2019,
+        # there will be a tiny bit of overage bc there's 1926 tons of "other" and 0 in 2018. ignoring for now
+        TRUE ~ catch_mt
+      )) %>%
+      filter (catch_mt > 0)
+    } # end peru if 
+    else {
+      input_landings <- sau_2019 %>%
+        filter (country == country_name) %>%
+        inner_join (priority_spp, by = c ("country", "species")) %>%
+        group_by (species, taxa, !!as.symbol(category)) %>%
+        summarise (catch_mt = sum (tonnes, na.rm = TRUE)) %>%
+        filter (catch_mt > 0)
+    }
+  } # end end_use if
+  
+  if (category == "fishing_country") {
+    input_landings <- sau_2019 %>%
+      filter (country == country_name) %>%
+      inner_join (priority_spp, by = c ("country", "species")) %>%
+      mutate (fishing_country = ifelse (fishing_entity == country_name, "Domestic catch", "Foreign catch")) %>%
+      group_by (species, taxa, !!as.symbol(category)) %>%
+      summarise (catch_mt = sum (tonnes, na.rm = TRUE)) %>%
+      filter (catch_mt > 0)
+  }
+  
+   input_landings %>%
+    mutate (children_fed = pmap (list (species = species, taxa = taxa, amount = catch_mt), calc_children_fed_func)) %>%
+    unnest(cols = c(children_fed),  names_repair = "check_unique") %>%
+    mutate(
+      spp_short = ifelse (
+        grepl(" ", species),
+        paste0 (substr(species, 1, 1), ". ", str_split_fixed (species, " ", 2)[,2]),
+        species) 
+    ) %>%
+    filter (!nutrient %in% c("Protein", "Selenium")) %>%
+    ggplot (aes (x = reorder(spp_short, -catch_mt), y = children_fed/1000000, fill = nutrient)) +
+    geom_col(position = "dodge") +
+    facet_wrap (as.formula(paste("~", category))) +
+    theme_bw() +
+    ggtitle (paste0("Child RNIs met from most recent year of landings, ", country_name)) +
+    labs (x = "", y = "Child RNIs met, millions", fill = "Nutrient") +
+    theme ( 
+      axis.text.y = element_text (size = 13),
+      axis.text.x = element_text (size = 11),
+      axis.title = element_text (size = 16),
+      strip.text = element_text(size = 16),
+      legend.text = element_text (size = 12),
+      legend.title = element_text (size = 14),
+      plot.title = element_text (size = 18))
+  
+}
+
+plot_sau_rnis_met_category(country_name = "Sierra Leone", category = "fishing_sector")
+plot_sau_rnis_met_category(country_name = "Peru", category = "end_use_type")
+plot_sau_rnis_met_category(country_name = "Sierra Leone", category = "fishing_country")
