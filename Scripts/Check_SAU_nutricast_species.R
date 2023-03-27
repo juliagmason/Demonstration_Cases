a438291c0bbd6e892652e406414c11f24b9b7b08
juliagmason <juliagmason@gmail.com>
2023-03-27 18:03:24 -0400

    Made two functions--one to check missing species, and one to take average nutricast upside ratios from family/genus. saved .RDS file with means for missing sau/chile species.
    this is with full code, will clean and delete extra

--- Scripts/Check_SAU_nutricast_species.R
+++ Scripts/Check_SAU_nutricast_species.R
@@ -4,8 +4,13 @@
 # JGM
 
 library (tidyverse)
-library (stringr)
+# https://rstats-tips.net/2020/07/31/get-rid-of-info-of-dplyr-when-grouping-summarise-regrouping-output-by-species-override-with-groups-argument/
+options(dplyr.summarise.inform = FALSE)
 
+
+library (stringr)
+#remotes::install_github("ropensci/rfishbase")
+library (rfishbase)
 
 # function for copying R output tables into word/excel----
 #https://stackoverflow.com/questions/24704344/copy-an-r-data-frame-to-an-excel-spreadsheet
@@ -25,6 +30,8 @@ write.excel <- function(x,row.names=FALSE,col.names=TRUE,...) {
 # so if we change nutricast to higher-level genus or family, we lose the nutrient data anyway. 
 # could change SAU data based on ratios in nutricast baseline?
 
+# SAU and fishnutr all matched now. so now should definitely match nutricast to SAU, and then mutiply children fed. 
+
 
 
 # nutricast
@@ -32,43 +39,156 @@ write.excel <- function(x,row.names=FALSE,col.names=TRUE,...) {
 # catch upside relative, from calculate_nutritional_upsides.r
 # expressed as catch ratios relative to base year for midcentury and end century, can multiply by landings
 catch_upside_relative <- readRDS("Data/nutricast_upside_relative.Rds")
+# 503 species, not all are in fishbase. get 481
+
+# match to family, get family info from fb?
+# species table just has family code. match from "families" table
+famcodes_fb <- fb_tbl("families") %>%
+  select ("FamCode", "Family")
+
+famcodes_slb <- fb_tbl("families", "sealifebase") %>%
+  select ("FamCode", "Family") 
+# these have overlap with fb
+
+
+nutricast_fams_fb <- fb_tbl ("species") %>%
+  mutate(species = paste(Genus, Species)) %>%
+  filter (species %in% catch_upside_relative$species) %>%
+  left_join (famcodes_fb, by = "FamCode") %>%
+  select (species, Genus, Family)
+
+nutricast_fams_slb <- fb_tbl ("species", "sealifebase") %>%
+  mutate(species = paste(Genus, Species)) %>%
+  filter (species %in% catch_upside_relative$species) %>%
+  left_join (famcodes_slb, by = "FamCode") %>%
+  select (species, Genus, Family)
+
+nutricast_fams <- rbind (nutricast_fams_fb, nutricast_fams_slb)
+
+catch_upside_relative_fam <- catch_upside_relative %>%
+  left_join (nutricast_fams, by = "species") %>%
+  # grab Genus even if not in fishbase
+  mutate (Genus = 
+            case_when (is.na (Genus) ~ word(species, 1),
+                       TRUE ~ Genus)
+  )
 
 # as of 10/25/22 just 2019 data, suggested by Deng Palomares. Clipped in SAU_explore.R
 # just grab species names
 sau_2019_taxa <- readRDS("Data/SAU_2019_taxa.Rds")
-
-# actually do need country?
 sau_2019_country_spp <- readRDS("Data/SAU_2019.Rds") %>%
   ungroup() %>%
   select (country, species) %>%
   distinct()
 
-sau_2019 <- readRDS("Data/SAU_2019.Rds")
+chl_landings <- readRDS ("Data/Chl_sernapesca_landings_compiled_2012_2021.Rds")
+chl_landings_spp <- chl_landings %>%
+  filter (year == 2021) %>%
+  mutate (country = "Chile") %>%
+  select (country, species) %>%
+  distinct()
+
+sau_peru <- sau_2019_country_spp %>% filter (country == "Peru")
+
+peru_nutricast_missing <- nutricast_peru %>% 
+  filter (!species %in% sau_peru$species)
+
+peru_sau_missing <- sau_peru %>%
+  filter (!species %in% nutricast_peru $species)
 
 
-# nutrient data ----
-fishnutr <- read_csv ("Data/Species_Nutrient_Predictions.csv")
 
+match_nutricast_taxa <- function (species_name, country_name) {
+  # send SAU species name into nutricast df
+  
+  catch_upside_country <- catch_upside_relative_fam %>%
+    filter (country == country_name) 
+  
+  # if identified to species level, clip to Genus
+  if (grepl(" ", species_name)) {species_name = word(species_name, 1)}
+  
+  if (grepl ("ae", str_sub(species_name, -2, -1))) {
+    match <- filter (catch_upside_country, Family == species_name)
+    
+    match_mean <- match %>%
+      group_by (rcp, Family) %>%
+      # take mean of all the ratio columns
+      summarise (across(bau_ratio_midcentury:adapt_ratio_endcentury, mean, na.rm = TRUE)) %>%
+      #rename (species = Family) %>%
+      # remove to avoid name duplication
+      select (-Family)
+     
+  
+  } else {
+    match <- filter (catch_upside_country, Genus == species_name)
+    
+    match_mean <- match %>%
+      group_by (rcp, Genus) %>%
+      # take mean of all the ratio columns
+      summarise (across(bau_ratio_midcentury:adapt_ratio_endcentury, mean, na.rm = TRUE)) %>%
+      #rename (species = Genus)
+      select (-Genus)
+    
+  }
 
+  
+  return (tibble(match_mean))
+  
+}
 
+match_nutricast_taxa("Caranx", "Peru")
 
+peru_match_taxa_nutricast <- peru_sau_missing %>%
+  mutate (nutricast = pmap(list(species_name = species, country = "Peru"), match_nutricast_taxa)
+  ) %>% 
+  unnest (cols = c(nutricast), names_repair = "check_unique")
 
-# which sau_2019 fish are not in fishnutr?
+length (unique (peru_match_taxa_nutricast$species)) # 17 of 65
 
-sau_missing_fish <- sau_2019_taxa %>%
-  filter (taxa == "Finfish", !species %in% fishnutr$species) %>%
-  left_join (sau_2019_country_spp, by = "species") %>%
-  select (species, country) %>%
-  mutate (level = case_when (
-    grepl ("ae", strsub(species, -2, -1))
+# can I combine missing and non-missing
+mega_peru <- sau_peru %>%
+  left_join (catch_upside_relative, by = c("country", "species")) %>%
+  mutate (nutricast = case_when (
+    is.na (rcp) ~ pmap(list(species_name = species, country = "Peru"), match_nutricast_taxa), 
+    TRUE ~ "NA"
   ))
+# no...
 
-# somehow have NA countries????
+# what is a smooth way of doing this all at once? making the missing data with 
+check_missing_nutricast_spp <- function (country_name) {
+  
+  if (country_name == "Chile") {
+    
+    landed_spp <- chl_landings_spp 
+  } else {
+    
+    landed_spp <- sau_2019_country_spp %>% filter (country == country_name)
+  }
+  
+  
+  nutricast_country <- catch_upside_relative %>% filter (country == country_name)
+  
+  landed_missing <- landed_spp %>%
+    filter (!species %in% nutricast_country$species) %>%
+    distinct()
+  
+  return (tibble(landed_missing))
+  
+}
+  
+i <- check_missing_nutricast_spp("Indonesia")
+c <- check_missing_nutricast_spp("Chile")
 
-View (sau_missing_fish)
 
-# Peru look at country-specific fishnutrients ----
 
+nutricast_missing_spp <- map_dfr(as.list (c("Chile", "Peru", "Indonesia", "Sierra Leone")), check_missing_nutricast_spp)
+
+nutricast_repair <- nutricast_missing_spp %>%
+ mutate (nutricast = pmap(list(species_name = species, country_name = country), match_nutricast_taxa)
+  ) %>% 
+    unnest (cols = c(nutricast), names_repair = "check_unique")
+
+saveRDS(nutricast_repair, file = "Data/catch_upside_relative_repair_missing.Rds")
 
 
 #######################################################################################
