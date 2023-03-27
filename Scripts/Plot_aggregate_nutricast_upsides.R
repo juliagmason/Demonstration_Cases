fe12e89ea5f945bfadd807389feaa0a4370b0fe4
juliagmason <juliagmason@gmail.com>
2023-03-27 18:36:16 -0400

    Brought in repaired species and made a function for plotting, seems to be working. committing now and will trim extra code.

--- Scripts/Plot_aggregate_nutricast_upsides.R
+++ Scripts/Plot_aggregate_nutricast_upsides.R
@@ -4,9 +4,17 @@
 
 library (tidyverse)
 
+# major update 3/27/23 is that I'm going to calculate children fed from current landings, and then multiply ratios for nutrient yield. doing this to preserve matched species. but maybe doesn't matter?
+
 # catch upside relative, from calculate_nutritional_upsides.r
 # expressed as catch ratios relative to base year for midcentury and end century, can multiply by landings
 catch_upside_relative <- readRDS("Data/nutricast_upside_relative.Rds")
+
+# averaged data for missing spp
+catch_upside_relative_missing <- readRDS("Data/catch_upside_relative_repair_missing.Rds")
+
+catch_upside_relative_repaired <- 
+  rbind (catch_upside_relative, catch_upside_relative_missing)
 
 # function for converting catch in mt to children fed ----
 # this will also bring in fishnutr data and RNI data
@@ -16,20 +24,88 @@ source ("Scripts/Function_convert_catch_amt_children_fed.R")
 # Chile country specific 
 chl_landings <- readRDS ("Data/Chl_sernapesca_landings_compiled_2012_2021.Rds")
 
-  
 
 # SAU data 
-
 # as of 10/25/22 just 2019 data, suggested by Deng Palomares. Clipped in SAU_explore.R
 sau_2019 <- readRDS("Data/SAU_2019.Rds") 
 
+
+# Function to convert landings to ratio to children fed, by country ----
+plot_nutr_upside_ratios <- function (country_name, Selenium = FALSE) {
+ 
+  if (country_name == "Chile") {
+    landings <- chl_landings %>%
+      filter (year == 2021) %>%
+      group_by (species, taxa) %>%
+      summarise (total_tonnes = sum (catch_mt)) %>%
+      mutate (country = "Chile")
+  } else {
+    
+    landings <- sau_2019 %>%
+      filter (country == country_name) %>%
+      left_join(sau_2019_taxa, by = "species") %>%
+      group_by (country, species, taxa) %>%
+      summarise (total_tonnes = sum (tonnes))
+  }
+  
+  upside_ratios <- landings %>% left_join(catch_upside_relative_repaired, by = c ("country", "species")) %>%
+    mutate (# multiply ratio by current landings
+      across(bau_ratio_midcentury:adapt_ratio_endcentury, ~.x * total_tonnes),
+      #convert to upside, subtract 
+      mey_2050 = mey_ratio_midcentury - bau_ratio_midcentury,
+      mey_2100 = mey_ratio_endcentury - bau_ratio_endcentury,
+      adapt_2050 = adapt_ratio_midcentury - bau_ratio_midcentury,
+      adapt_2100 = adapt_ratio_endcentury - bau_ratio_endcentury) %>%
+    
+    select (country, rcp, species, taxa, mey_2050:adapt_2100) %>%
+    pivot_longer(mey_2050:adapt_2100, 
+                 names_to = "upside",
+                 values_to = "tonnes") %>%
+    # get rid of non-matching species, NAs
+    filter (!is.na (rcp)) %>%
+    # convert to nutrients
+    mutate (children_fed = pmap (list (species = species, taxa = taxa, amount = tonnes, country = "Peru"), calc_children_fed_func)) %>%
+    unnest(cols = c(children_fed),  names_repair = "check_unique")
+  
+  # fix ratios
+  upside_ratios$upside <- factor(upside_ratios$upside, levels = c ("mey_2050", "mey_2100", "adapt_2050", "adapt_2100"))
+  
+  if (Selenium == TRUE) {
+    upside_summary <- upside_ratios %>%
+      group_by (rcp, upside, nutrient) %>%
+      summarise (total_fed = sum (children_fed, na.rm = TRUE)) %>%
+      filter (!nutrient %in% c("Protein"),
+              upside %in% c("mey_2050", "adapt_2050")) 
+  } else {
+    
+    upside_summary <- upside_ratios %>%
+      group_by (rcp, upside, nutrient) %>%
+      summarise (total_fed = sum (children_fed, na.rm = TRUE)) %>%
+      filter (!nutrient %in% c("Protein", "Selenium"),
+              upside %in% c("mey_2050", "adapt_2050")) 
+  }
+  
+    upside_summary %>%
+    # preliminary plot
+    ggplot (aes (x = reorder(nutrient, -total_fed), y = total_fed/1000000, fill = upside)) +
+    geom_col (position = "dodge") +
+    geom_hline (yintercept = 0, lty = 2) +
+    facet_wrap ( ~ rcp) +
+    theme_bw() +
+    # roughly match colors from gaines et al
+    scale_fill_manual (values = c ("mediumseagreen", "dodgerblue4")) +
+    labs (y = "Change in # children fed, millions", x = "", fill = "Management \nstrategy") +
+    ggtitle ("Nutrition upside from climate-adaptive management")
+}
+
+plot_nutr_upside_ratios("Peru")
 
 # Chile ----
 
 nutricast_chile <- catch_upside_relative %>% filter (country == "Chile")
 # lose 10 species from 2021 data...should be more
 View (sort(unique (nutricast_chile$species)))
-unique (chl_landings$species[which(!chl_landings$species %in% nutricast_chile$species)])
+length(unique (chl_landings$species[which(!chl_landings$species %in% nutricast_chile$species)])) # 98
 
 upside_ratios_chl <- chl_landings %>%
   filter (year == 2021) %>%
@@ -129,7 +205,7 @@ upside_ratios_peru <- sau_2019 %>%
   left_join(sau_2019_taxa, by = "species") %>%
   group_by (country, species, taxa) %>%
   summarise (total_tonnes = sum (tonnes)) %>%
-  left_join(catch_upside_relative, by = c ("country", "species")) %>%
+  left_join(catch_upside_relative_repaired, by = c ("country", "species")) %>%
   mutate (# multiply ratio by current landings
     across(bau_ratio_midcentury:adapt_ratio_endcentury, ~.x * total_tonnes),
     #convert to upside, subtract 
@@ -145,10 +221,11 @@ upside_ratios_peru <- sau_2019 %>%
   # get rid of non-matching species, NAs
   filter (!is.na (rcp)) %>%
   # convert to nutrients
-  mutate (children_fed = pmap (list (species = species, taxa = taxa, amount = tonnes), calc_children_fed_func)) %>%
+  mutate (children_fed = pmap (list (species = species, taxa = taxa, amount = tonnes, country = "Peru"), calc_children_fed_func)) %>%
   unnest(cols = c(children_fed),  names_repair = "check_unique")
 
 upside_ratios_peru$upside <- factor(upside_ratios_peru$upside, levels = c ("mey_2050", "mey_2100", "adapt_2050", "adapt_2100"))
+
 
 png ("Figures/Peru_nutricast_upside_overall.png", width = 6, height = 5, units= "in", res = 300)
 upside_ratios_peru %>%
@@ -161,9 +238,66 @@ upside_ratios_peru %>%
   geom_hline (yintercept = 0, lty = 2) +
   facet_wrap ( ~ rcp) +
   theme_bw() +
+  # roughly match colors from gaines et al
+  scale_fill_manual (values = c ("mediumseagreen", "dodgerblue4")) +
   labs (y = "Change in # children fed, millions", x = "", fill = "Management \nstrategy") +
-  ggtitle ("Nutrition upside from climate-adaptive management, Peru") +
-  theme (plot.title = element_text (size = 18),
+  ggtitle ("Nutrition upside from climate-adaptive management") +
+  theme (plot.title = element_text (size = 17),
+         axis.text = element_text (size = 11),
+         axis.text.x = element_text (angle = 60, hjust = 1),
+         strip.text.x =  element_text (size = 12),
+         axis.title = element_text (size = 16),
+         legend.title = element_text (size = 14),
+         legend.text = element_text (size = 11)) 
+dev.off()
+
+# Indonesia ----
+
+nutricast_indo<- catch_upside_relative_repaired %>% filter (country == "Indonesia")
+
+upside_ratios_indo <- sau_2019 %>%
+  filter (country == "Indonesia") %>%
+  left_join(sau_2019_taxa, by = "species") %>%
+  group_by (country, species, taxa) %>%
+  summarise (total_tonnes = sum (tonnes)) %>%
+  left_join(catch_upside_relative_repaired, by = c ("country", "species")) %>%
+  mutate (# multiply ratio by current landings
+    across(bau_ratio_midcentury:adapt_ratio_endcentury, ~.x * total_tonnes),
+    #convert to upside, subtract 
+    mey_2050 = mey_ratio_midcentury - bau_ratio_midcentury,
+    mey_2100 = mey_ratio_endcentury - bau_ratio_endcentury,
+    adapt_2050 = adapt_ratio_midcentury - bau_ratio_midcentury,
+    adapt_2100 = adapt_ratio_endcentury - bau_ratio_endcentury) %>%
+  
+  select (country, rcp, species, taxa, mey_2050:adapt_2100) %>%
+  pivot_longer(mey_2050:adapt_2100, 
+               names_to = "upside",
+               values_to = "tonnes") %>%
+  # get rid of non-matching species, NAs
+  filter (!is.na (rcp)) %>%
+  # convert to nutrients
+  mutate (children_fed = pmap (list (species = species, taxa = taxa, amount = tonnes, country = "Peru"), calc_children_fed_func)) %>%
+  unnest(cols = c(children_fed),  names_repair = "check_unique")
+
+upside_ratios_indo$upside <- factor(upside_ratios_indo$upside, levels = c ("mey_2050", "mey_2100", "adapt_2050", "adapt_2100"))
+
+upside_ratios_indo1 <- upside_ratios_indo
+png ("Figures/Peru_nutricast_upside_overall.png", width = 6, height = 5, units= "in", res = 300)
+upside_ratios_indo %>%
+  group_by (rcp, upside, nutrient) %>%
+  summarise (total_fed = sum (children_fed, na.rm = TRUE)) %>%
+  filter (!nutrient %in% c("Protein", "Selenium"),
+          upside %in% c("mey_2050", "adapt_2050")) %>%
+  ggplot (aes (x = nutrient, y = total_fed/1000000, fill = upside)) +
+  geom_col (position = "dodge") +
+  geom_hline (yintercept = 0, lty = 2) +
+  facet_wrap ( ~ rcp) +
+  theme_bw() +
+  # roughly match colors from gaines et al
+  scale_fill_manual (values = c ("mediumseagreen", "dodgerblue4")) +
+  labs (y = "Change in # children fed, millions", x = "", fill = "Management \nstrategy") +
+  ggtitle ("Nutrition upside from climate-adaptive management") +
+  theme (plot.title = element_text (size = 17),
          axis.text = element_text (size = 11),
          axis.text.x = element_text (angle = 60, hjust = 1),
          strip.text.x =  element_text (size = 12),
