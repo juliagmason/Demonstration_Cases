bcd0101d209754c4969e0a6bcdce7961db5e5c7d
juliagmason <juliagmason@gmail.com>
2023-03-29 18:50:03 -0400

    using compiled nutrient data. may need to also bring in matched nutrients if needed. removed older code with species-specific nutrients

--- Scripts/Plot_nutrient_content_dodged_bar.R
+++ Scripts/Plot_nutrient_content_dodged_bar.R
@@ -18,58 +18,8 @@ priority_spp <- read_csv ("Data/regional_teams_priority_spp.csv") %>%
 
 # nutrient data ----
 
-# fish nutrients models, ran code from https://github.com/mamacneil/NutrientFishbase
-# this is per 100g raw, edible portion
-fishnutr <- read_csv ("Data/Species_Nutrient_Predictions.csv")
-
-# truncate to just summary predicted value. eventually will want range?
-fishnutr_mu <- fishnutr %>%
-  select (species, ends_with ("_mu"))
-
-
-fishnutr_long <- fishnutr_mu %>% 
-  pivot_longer (Selenium_mu:Vitamin_A_mu,
-                names_to = "nutrient",
-                values_to = "amount") %>%
-  mutate (nutrient = str_sub(nutrient, end = -4))
-
-# d gigas data from Bianchi et al. 2022 supp table 2. Vita A is retinol equiv; omega 3 is n-3 fatty acids
-d_gigas_nutr <- data.frame (
-  species = "Dosidicus gigas", 
-  nutrient = c ("Calcium", "Iron", "Omega_3", "Protein", "Selenium", "Vitamin_A", "Zinc"),
-  amount = c(37.5, 3.3, 0.6, 16.4, 50.9, 0, 2.8)
-)
-
-# scylla serrata for indonesia
-# picking somewhat randomly. note that there's a separate dha + epa for omegas, and different vitamin As
-
-s_serrata_nutr <- afcd_sci %>% 
-  filter (sciname == "Scylla serrata",
-          nutrient_code_fao %in% c(
-            "CA", "ZN", "FE", "SE", "Protein", "FAPU", "VITA")) %>%
-  group_by(nutrient_code_fao) %>%
-  summarise (amount = mean (value, na.rm = TRUE)) %>%
-  mutate (nutrient =
-            case_when (nutrient_code_fao == "CA" ~ "Calcium",
-                       nutrient_code_fao == "FE" ~ "Iron",
-                       nutrient_code_fao == "SE" ~ "Selenium", 
-                       nutrient_code_fao == "ZN" ~ "Zinc",
-                       nutrient_code_fao == "FAPU" ~ "Omega_3",
-                       nutrient_code_fao == "VITA" ~ "Vitamin_A"),
-          species = "Scylla serrata"
-  ) %>%
-  # reorder to match
-  select (species, nutrient, amount)
-
-# composite Stolephorus spp for Indonesia
-indo_stolephorus <- readRDS("Data/indo_stolephorus.Rds")
-
-stoleph_nutr <- fishnutr_long %>%
-  filter (species %in% indo_stolephorus) %>%
-  group_by (nutrient) %>%
-  summarise (amount = mean (amount)) %>%
-  mutate (species = "Stolephorus") %>%
-  select (species, nutrient, amount)
+# bring in compiled instead; compile_species_nutrition_data.R
+compiled_nutr <- readRDS("Data/species_nutrients_compiled.Rds")
 
 
 # RNI data ----
@@ -78,20 +28,13 @@ rni_child <- readRDS("Data/RNI_child.Rds")
 
 
 # join nutrients and rni ----
-
-pri_spp_nutr <- fishnutr_long %>%
-  rbind (d_gigas_nutr) %>%
-  rbind (s_serrata_nutr) %>%
-  rbind (stoleph_nutr) %>%
-  right_join (priority_spp, by = "species")  %>%
-  
-  
+spp_nutr <-  compiled_nutr %>%
   # join to rni data
   left_join (rni_child, by = "nutrient") %>%
   
   # this would be the percentage of your daily requirement you could get from a 100g serving of each species. cap at 100%
   mutate (perc_rni = amount/RNI * 100,
-          perc_rni = ifelse (perc_rni > 100, 100, perc_rni),
+          #perc_rni = ifelse (perc_rni > 100, 100, perc_rni),
           nutrient = 
             case_when (nutrient == "Vitamin_A" ~ "Vit A",
                        nutrient == "Omega_3" ~ "Omega 3",
@@ -107,10 +50,14 @@ pri_spp_nutr <- fishnutr_long %>%
 # https://stackoverflow.com/questions/8299978/splitting-a-string-on-the-first-space
 
 
-plot_colorful_spp_nutr_dodge_bar <- function (country_name) {
-  pri_spp_nutr %>%
-    filter (!nutrient %in% c("Protein", "Selenium"), 
-            country == country_name) %>%
+plot_colorful_spp_nutr_dodge_bar <- function (species_names, Selenium = FALSE) {
+  #species_names is a vector of scientific names
+  
+  if (Selenium == TRUE) {omit_nutrients <- "Protein"} else {omit_nutrients <- c("Protein", "Selenium")}
+  
+  spp_nutr %>%
+    filter (!nutrient %in% omit_nutrients, 
+            species %in% species_names) %>%
     group_by (species) %>%
     mutate (micronutrient_density = sum (perc_rni),
             spp_short = ifelse (
@@ -124,8 +71,8 @@ plot_colorful_spp_nutr_dodge_bar <- function (country_name) {
     geom_col (position = "dodge") +
     theme_bw() +
     labs (x = "", y = "% Child RNI met per 100g serving", fill = "Nutrient") +
-    ylim (c(0,100)) +
-    ggtitle (paste0("Nutrient content of selected species, ", country_name)) +
+    #ylim (c(0,100)) +
+    ggtitle ("Nutrient content of selected species") +
     theme ( 
       axis.text.y = element_text (size = 13),
       axis.text.x = element_text (size = 11),
@@ -135,6 +82,25 @@ plot_colorful_spp_nutr_dodge_bar <- function (country_name) {
       legend.title = element_text (size = 14),
       plot.title = element_text (size = 18))
 }
+
+# try to find high selenium indo??
+indo_spp <- sau_2019 %>%
+  filter (country == "Indonesia") %>%
+  group_by (species) %>%
+  summarise (tot_catch = sum (tonnes)) %>%
+  top_n(20, tot_catch)
+
+plot_colorful_spp_nutr_dodge_bar(indo_spp$species, Selenium = TRUE)
+# Sardinella species, and scomberomorous
+
+# quick peru plot
+peru_spp <- priority_spp %>% filter (country == "Peru", rank <6)
+png ("Figures/Peru_pri_spp_nutr_dodge_bar_sm.png", width = 7, height = 4, units = "in", res = 300)
+print (
+  plot_colorful_spp_nutr_dodge_bar(peru_spp$species, Selenium = FALSE)
+)
+dev.off()
+
 
 png ("Figures/SL_pri_spp_nutr_dodge_bar.png", width = 10, height = 5, units = "in", res = 300)
 print (
