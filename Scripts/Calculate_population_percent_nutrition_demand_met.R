027daf5ab827a2a950b4e3a4a840d4e6242d7300
juliagmason <juliagmason@gmail.com>
2023-05-23 16:08:49 -0400

    Cleaned setup code; ran bar and 3 step plots for indo

--- Scripts/Calculate_population_percent_nutrition_demand_met.R
+++ Scripts/Calculate_population_percent_nutrition_demand_met.R
@@ -23,120 +23,122 @@ rni <- rni %>%
           Riboflavin_mg_day = c(rep(c(0.3, 0.4, 0.5, 0.6, 0.9), each = 2), c(1, 1.3, 1.1, 1.1, 1.3, 1.3, 1.1, 1.3), rep (c(1.4, 1.6), each = 3))
   )
 
-# updated WPP data
-# https://population.un.org/wpp/Download/Standard/CSV/
-# chose option population -1 January by 5 yr age groups, medium variant
-# numbers are in 1000s
-wpp_pop <- read_csv("Data/WPP2022_Population1JanuaryByAge5GroupSex_Medium.csv")
 
-
-# Build age matching key----
-# not perfect. take average RNI for 0-3 years to match 0-4
-# for MOST micronutrients, level for 7-9 is higher than 4-6. so using 7-9 level for ages 5-9 is conservative.
-
-# first do avg of rni for infants? probably a better way to do this
-rni_merge_infants <- rni %>%
-  mutate (age_infant = case_when (
-    Age %in% c("0-6 months", "7-12 months","1-3 years") ~ "0-3",
-    TRUE ~ Age
-  )) %>% 
-  group_by (Sex, age_infant) %>%
-  #summarise (across(Calcium_mg_day:Omega3_PUFA_g_day, ~mean(.x, na.rm = TRUE))) %>%
-  summarise (across(Calcium_mg_day:Riboflavin_mg_day, ~mean(.x, na.rm = TRUE))) %>%
-  # rename age range to match
-  rename (age_range_rni = age_infant) %>%
-  # pivot longer
-  pivot_longer(Calcium_mg_day:Riboflavin_mg_day,
-               names_to = "nutrient",
-               values_to = "amount") %>%
-  # fix names
-  mutate (nutrient = sub("_.*", "", nutrient),
-          nutrient = case_when (nutrient == "VitaminA" ~ "Vitamin_A",
-                                nutrient == "Omega3" ~ "Omega_3",
-                              TRUE ~ nutrient )) %>%
-  filter (!nutrient == "fish")
-
-
-age_range_key <- tibble(age_range_wpp=unique(wpp_pop$AgeGrp)) %>% 
-  mutate(age_range_wpp=factor(age_range_wpp, levels = c ("0-4",
-                                                         "5-9",
-                                                         "10-14",
-                                                         "15-19",
-                                                         "20-24",
-                                                         "25-29",
-                                                         "30-34",
-                                                         "35-39",
-                                                         "40-44",
-                                                         "45-49",
-                                                         "50-54",
-                                                         "55-59",
-                                                         "60-64",
-                                                         "65-69",
-                                                         "70-74",
-                                                         "75-79",
-                                                         "80-84",
-                                                         "85-89",
-                                                         "90-94",
-                                                         "95-99",
-                                                         "100+")),
-         age_range_rni = case_when(
-           age_range_wpp == "0-4" ~ "0-3",
-           age_range_wpp ==   "5-9" ~ "7-9 years",
-           age_range_wpp %in% c("10-14", "15-19") ~ "10-18 years",
-           age_range_wpp %in% c("20-24",
-                                "25-29",
-                                "30-34",
-                                "35-39",
-                                "40-44",
-                                "45-49")~ "19-50 years",
-           age_range_wpp %in% c("50-54",
-                                "55-59",
-                                "60-64") ~ "51-65 years",
-           age_range_wpp %in% c("65-69",
-                                "70-74",
-                                "75-79",
-                                "80-84",
-                                "85-89",
-                                "90-94",
-                                "95-99",
-                                "100+") ~  "65+"
-  ))
-
-
-# calculate annual nutrition demand ----
-# reshape and join population data to 
-wpp_long <- wpp_pop %>%
-  filter (!is.na(ISO3_code)) %>%
-  select (Location, ISO3_code, Time, AgeGrp, PopMale, PopFemale) %>%
-  rename (country = Location, age_range_wpp = AgeGrp) %>%
-  pivot_longer (PopMale:PopFemale,
-                names_prefix = "Pop",
-                names_to = "Sex",
-                values_to = "Pop") %>%
-  # cut to first letter to match rni
-  mutate (Sex = substr(Sex, 1, 1)) %>%
-  # join to age key
-  left_join (age_range_key, by = "age_range_wpp") %>%
-  # join to rni data
-  left_join (rni_merge_infants, by = c ("Sex", "age_range_rni")) %>%
-  # multiply population by amount needed. this is in native units so need to convert to tons
-  mutate (scalar = case_when (
-            nutrient %in% c("Protein", "Omega_3") ~ 1,
-            nutrient %in% c("Calcium", "Zinc", "Iron", "Riboflavin") ~ 1/1000,
-            nutrient %in% c("Vitamin_A", "Selenium", "Folate") ~ 1/1e6),
-          #multiply population by 1000
-          nutr_annual_demand = Pop * 1000 * amount * scalar/1000/1000*365) 
-          
-
-saveRDS(wpp_long, "Data/annual_nutr_demand_rni_by_country_age_sex.Rds")
-
-# also calculate total for each country
-wpp_country_aggregate <- wpp_long %>%
-  group_by (country, Time, nutrient) %>%
-  summarise (tot_pop = sum (Pop),
-             tot_nutr_annual_demand = sum (nutr_annual_demand))
-
-saveRDS(wpp_country_aggregate, "Data/annual_nutr_demand_rni_by_country.Rds")
+# 
+# # updated WPP data
+# # https://population.un.org/wpp/Download/Standard/CSV/
+# # chose option population -1 January by 5 yr age groups, medium variant
+# # numbers are in 1000s
+# wpp_pop <- read_csv("Data/WPP2022_Population1JanuaryByAge5GroupSex_Medium.csv")
+# 
+# 
+# # Build age matching key----
+# # not perfect. take average RNI for 0-3 years to match 0-4
+# # for MOST micronutrients, level for 7-9 is higher than 4-6. so using 7-9 level for ages 5-9 is conservative.
+# 
+# # first do avg of rni for infants? probably a better way to do this
+# rni_merge_infants <- rni %>%
+#   mutate (age_infant = case_when (
+#     Age %in% c("0-6 months", "7-12 months","1-3 years") ~ "0-3",
+#     TRUE ~ Age
+#   )) %>% 
+#   group_by (Sex, age_infant) %>%
+#   #summarise (across(Calcium_mg_day:Omega3_PUFA_g_day, ~mean(.x, na.rm = TRUE))) %>%
+#   summarise (across(Calcium_mg_day:Riboflavin_mg_day, ~mean(.x, na.rm = TRUE))) %>%
+#   # rename age range to match
+#   rename (age_range_rni = age_infant) %>%
+#   # pivot longer
+#   pivot_longer(Calcium_mg_day:Riboflavin_mg_day,
+#                names_to = "nutrient",
+#                values_to = "amount") %>%
+#   # fix names
+#   mutate (nutrient = sub("_.*", "", nutrient),
+#           nutrient = case_when (nutrient == "VitaminA" ~ "Vitamin_A",
+#                                 nutrient == "Omega3" ~ "Omega_3",
+#                               TRUE ~ nutrient )) %>%
+#   filter (!nutrient == "fish")
+# 
+# 
+# age_range_key <- tibble(age_range_wpp=unique(wpp_pop$AgeGrp)) %>% 
+#   mutate(age_range_wpp=factor(age_range_wpp, levels = c ("0-4",
+#                                                          "5-9",
+#                                                          "10-14",
+#                                                          "15-19",
+#                                                          "20-24",
+#                                                          "25-29",
+#                                                          "30-34",
+#                                                          "35-39",
+#                                                          "40-44",
+#                                                          "45-49",
+#                                                          "50-54",
+#                                                          "55-59",
+#                                                          "60-64",
+#                                                          "65-69",
+#                                                          "70-74",
+#                                                          "75-79",
+#                                                          "80-84",
+#                                                          "85-89",
+#                                                          "90-94",
+#                                                          "95-99",
+#                                                          "100+")),
+#          age_range_rni = case_when(
+#            age_range_wpp == "0-4" ~ "0-3",
+#            age_range_wpp ==   "5-9" ~ "7-9 years",
+#            age_range_wpp %in% c("10-14", "15-19") ~ "10-18 years",
+#            age_range_wpp %in% c("20-24",
+#                                 "25-29",
+#                                 "30-34",
+#                                 "35-39",
+#                                 "40-44",
+#                                 "45-49")~ "19-50 years",
+#            age_range_wpp %in% c("50-54",
+#                                 "55-59",
+#                                 "60-64") ~ "51-65 years",
+#            age_range_wpp %in% c("65-69",
+#                                 "70-74",
+#                                 "75-79",
+#                                 "80-84",
+#                                 "85-89",
+#                                 "90-94",
+#                                 "95-99",
+#                                 "100+") ~  "65+"
+#   ))
+# 
+# 
+# # calculate annual nutrition demand ----
+# # reshape and join population data to 
+# wpp_long <- wpp_pop %>%
+#   filter (!is.na(ISO3_code)) %>%
+#   select (Location, ISO3_code, Time, AgeGrp, PopMale, PopFemale) %>%
+#   rename (country = Location, age_range_wpp = AgeGrp) %>%
+#   pivot_longer (PopMale:PopFemale,
+#                 names_prefix = "Pop",
+#                 names_to = "Sex",
+#                 values_to = "Pop") %>%
+#   # cut to first letter to match rni
+#   mutate (Sex = substr(Sex, 1, 1)) %>%
+#   # join to age key
+#   left_join (age_range_key, by = "age_range_wpp") %>%
+#   # join to rni data
+#   left_join (rni_merge_infants, by = c ("Sex", "age_range_rni")) %>%
+#   # multiply population by amount needed. this is in native units so need to convert to tons
+#   mutate (scalar = case_when (
+#             nutrient %in% c("Protein", "Omega_3") ~ 1,
+#             nutrient %in% c("Calcium", "Zinc", "Iron", "Riboflavin") ~ 1/1000,
+#             nutrient %in% c("Vitamin_A", "Selenium", "Folate") ~ 1/1e6),
+#           #multiply population by 1000
+#           nutr_annual_demand = Pop * 1000 * amount * scalar/1000/1000*365) 
+#           
+# 
+# saveRDS(wpp_long, "Data/annual_nutr_demand_rni_by_country_age_sex.Rds")
+# 
+# # also calculate total for each country
+# wpp_country_aggregate <- wpp_long %>%
+#   group_by (country, Time, nutrient) %>%
+#   summarise (tot_pop = sum (Pop),
+#              tot_nutr_annual_demand = sum (nutr_annual_demand))
+# 
+# saveRDS(wpp_country_aggregate, "Data/annual_nutr_demand_rni_by_country.Rds")
 
 
 # relate landings to nutrition demand ----
@@ -172,13 +174,15 @@ convert_catch_to_nutr_tons <- function (species_name, catch_mt, country_name) {
   
   catch_nutrients <- nutr_content %>%
     mutate (
-      p_edible = case_when (
-        taxa == "Finfish" ~ 0.87,
-        taxa == "Crustacean" ~ 0.36,
-        taxa == "Mollusc" ~ 0.17,
-        # GENuS/nutricast is 0.21 for cephalopods. Using 0.67, Bianchi et al. 2022 value for D. gigas; only cephalopod in our priority species. They also have a blanket 0.7 value for cephalopods.  
-        taxa == "Cephalopod" ~ 0.67,
-        taxa == "Other" ~ 1),
+            p_edible = case_when (
+              taxa == "Finfish" ~ 0.87,
+              taxa == "Crustacean" ~ 0.36,
+              taxa == "Mollusc" ~ 0.17,
+              # GENuS/nutricast is 0.21 for cephalopods. Using 0.67, Bianchi et al. 2022 value for D. gigas; only cephalopod in our priority species. They also have a blanket 0.7 value for cephalopods.  
+              taxa == "Cephalopod" & species == "Dosidicus gigas" ~ 0.67,
+              taxa == "Cephalopod" & species != "Dosidicus gigas" ~ 0.21,
+              taxa == "Other" ~ 1,
+              taxa == "Algae" ~ 1),
       # have to put back into units
       scalar = case_when (
         nutrient %in% c("Protein", "Omega_3") ~ 1,
@@ -273,6 +277,28 @@ a %>%
 b <- calculate_prop_demand_met("Peru", 2019) # seems to match
 
 
+# Indonesia
+indo_pop_needs <- sau_2019 %>%
+  filter(country == "Indonesia") %>%
+  left_join (sau_2019_taxa, by = "species") %>%
+  group_by (country, species, commercial_group) %>%
+  summarise (catch_mt = sum (tonnes, na.rm = TRUE)) %>%
+  mutate (nutr_tonnes = pmap (list (species_name = species, catch_mt = catch_mt, country_name = "Peru"), convert_catch_to_nutr_tons)) %>%
+  unnest(cols = c(nutr_tonnes),  names_repair = "check_unique") %>%
+  left_join (filter(wpp_country_aggregate, Time == 2019), by = c("nutrient", "country")) %>%
+  mutate (prop_demand_met = nutr_tonnes / tot_nutr_annual_demand)
+
+indo_pop_needs %>%
+  group_by (nutrient, commercial_group) %>%
+  filter (!nutrient %in% c("Selenium")) %>%
+  summarise (prop_demand_met = sum (prop_demand_met, na.rm = TRUE)) %>%
+  ggplot (aes (x = reorder(nutrient, -prop_demand_met, na.rm = TRUE), y = prop_demand_met*100, fill = commercial_group)) +
+  geom_col() +
+  theme_bw() +
+  ggtitle ("Proportion demand met\nMost recent year of landings, Indonesia") +
+  labs (x = "", y = "% population RNIs met", fill = "Comm. group") 
+
+
 # plot nutricast upsides ----
 catch_upside_relative <- readRDS("Data/nutricast_upside_relative.Rds")
 
@@ -295,7 +321,7 @@ wpp_nutricast_periods <- wpp_country_aggregate %>%
   
 
 
-plot_nutr_upside_absolute_population <- function (country_name, Selenium = FALSE) {
+calc_nutr_upside_absolute_population <- function (country_name, Selenium = FALSE) {
   
   if (country_name == "Chile") {
     landings <- chl_landings %>%
@@ -345,7 +371,7 @@ plot_nutr_upside_absolute_population <- function (country_name, Selenium = FALSE
     
     upside_summary <- upside_ratios_absolute %>%
       group_by (country, rcp, scenario, period, nutrient) %>%
-      summarise (total_yield = sum (children_yield, na.rm = TRUE)) %>%
+      summarise (total_yield = sum (nutr_tonnes, na.rm = TRUE)) %>%
       filter (!nutrient %in% c("Protein", "Selenium")) 
   }
   
@@ -354,16 +380,66 @@ plot_nutr_upside_absolute_population <- function (country_name, Selenium = FALSE
  prop_met <-  upside_summary %>%
     left_join (wpp_nutricast_periods, by = c ("country", "period", "nutrient")) %>%
     mutate (prop_met = total_yield/mean_annual_demand)
-  
-  prop_met %>%
+ 
+}
+
+indo_pop_needs_future <- calc_nutr_upside_absolute_population("Indonesia")
+
+
+# plot as bar  
+indo_pop_needs_future %>%
     # preliminary plot
-    ggplot (aes (x = reorder(nutrient, -total_yield), y = prop_met * 100, fill = scenario)) +
+    #ggplot (aes (x = reorder(nutrient, -total_yield), y = prop_met * 100, fill = scenario)) +
+  ggplot (aes (x = nutrient, y = prop_met * 100, fill = scenario)) +
     geom_col (position = "dodge") +
     geom_hline (yintercept = 0, lty = 2) +
     facet_wrap (period ~ rcp, ncol =4) +
     theme_bw() +
     # roughly match colors from gaines et al
     scale_fill_manual (values = c ("firebrick", "mediumseagreen", "dodgerblue4")) +
-    labs (y = "# Child RNIs met, millions", x = "", fill = "Management \nstrategy") +
+    labs (y = "% population RNI equivalents", x = "", fill = "Management \nstrategy") +
     ggtitle ("Nutrition provisioning from climate-adaptive management")
+
+
+# plot as 3 step
+
+
+indo_pop_needs_future %>%
+  filter (rcp == "RCP60") %>%
+  ggplot (aes (x = factor(period), y = prop_met * 100, col = scenario, group = scenario)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap (~nutrient) +
+  theme_bw()
+
+
+
+plot_nutr_absolutes_tonnes <- function (country_name) {
+  
+  upsides <- calc_nutr_upside_tonnes(country_name)
+  
+  q <- upsides %>%
+    group_by (country, rcp, scenario, period, nutrient) %>%
+    summarise (tot_fed = sum (children_fed, na.rm = TRUE)) %>%
+    filter (rcp == "RCP60", !nutrient == "Protein") %>%
+    filter (!nutrient == "Protein") %>%
+    ggplot (aes (x = factor(period), y = tot_fed/1000000, col = scenario, group = scenario)) +
+    geom_point() +
+    geom_line() +
+    facet_wrap (~nutrient) +
+    theme_bw() +
+    labs (y = "Child RNI equivalents, millions", x = "", col = "Mgmt\nscenario") +
+    ggtitle (paste0 ("Projected nutrient yield for ", country_name, ", RCP 6.0"))
+  
 }
+
+
+a <- plot_nutr_absolutes_tonnes("Peru")
+
+png("Figures/nutricast_3pt_ts_Peru_free.png", width = 6.5, height = 4, units = "in", res = 300)
+a + 
+  facet_wrap (~ nutrient, scales = "free_y") +
+  scale_x_discrete (labels = c ("current", "2050s", "2090s")) +
+  theme (axis.text = element_text (size = 10),
+         axis.title = element_text (size = 14)) 
+dev.off()
