0047b6566d53df0c78c85d99974e5abcfa81b014
juliagmason <juliagmason@gmail.com>
2023-05-23 14:31:06 -0400

    successful sankeys with ggalluvial!! did for peru and indonesia

--- Scripts/Plot_aggregate_allocative_losses.R
+++ Scripts/Plot_aggregate_allocative_losses.R
@@ -36,6 +36,11 @@ peru_anchov_total_2019 <- sau_2015_2019 %>%
   filter (country == "Peru", fishing_entity == "Peru", year == 2019, species == "Engraulis ringens") %>%
   pull (tonnes) %>% sum()
 
+# do for foreign
+peru_anchov_total_2019_foreign <- sau_2015_2019 %>%
+  filter (country == "Peru", fishing_entity != "Peru", year == 2019, species == "Engraulis ringens") %>%
+  pull (tonnes) %>% sum()
+
 # exports ARTIS ----
 # placeholder for now; need full exports
 # emailed 10 19 2022
@@ -229,6 +234,157 @@ plot_ly(
   layout(title = "Sierra Leone Nutrient Flows",
          font = list(size = 14),
          margin = list(t = 40, l = 10, r = 10, b = 10))
+
+
+## ggalluvial??
+#https://corybrunson.github.io/ggalluvial/
+
+library (ggalluvial)
+
+# following sl methods from above to join
+
+indo_sau_exports <- sau_2019 %>%
+  filter (country == "Indonesia", fishing_entity == "Indonesia") %>%
+  left_join (exports_5yr_mean, by = c("species", "country")) %>%
+  replace_na (list(mn_prop_exp = 0)) %>%
+  mutate (Exported = tonnes * mn_prop_exp,
+          Kept = tonnes * (1-mn_prop_exp)) %>%
+  ungroup() %>%
+  select (species, Exported, Kept) %>%
+  pivot_longer (-species,
+                names_to = "trade", 
+                values_to = "tonnes") %>%
+  mutate (fleet = "Domestic")
+  
+indo_trade <- sau_2019 %>%
+  filter (country == "Indonesia") %>%
+  # need different column for foreign/domestic and exported
+  mutate (fleet = case_when (fishing_entity == "Indonesia" ~ "Domestic",
+                             fishing_entity != "Indonesia" ~ "Foreign fishing")
+  ) %>%
+  group_by (country, species, fleet) %>%
+  summarise (tonnes2 = sum (tonnes)) %>%
+  left_join (indo_sau_exports, by = c ("species", "fleet")) %>%
+  mutate (tonnes = ifelse (is.na (tonnes), tonnes2, tonnes),
+          rni_equivalents = pmap (list (species = species, amount = tonnes, country_name = "Indonesia"), calc_children_fed_func)) %>%
+  unnest (cols = c(rni_equivalents))  %>%
+  group_by (fleet, trade, nutrient) %>%
+  summarise (tot_fed= sum (rni_equivalents, na.rm = TRUE)) 
+
+
+# this one keeps NAs, so maybe just say foreign fishing is exported?
+indo_trade <- indo_trade %>%
+  mutate (trade = case_when (fleet == "Foreign fishing" ~ "Exported", TRUE ~ trade))
+
+indo_trade$trade <- factor (indo_trade$trade, levels = c("Kept", "Exported"))
+
+indo_trade %>%
+  filter (!nutrient %in% c("Protein", "Selenium")) %>%
+  ggplot (aes (axis1 = nutrient, 
+               axis2 = fleet,
+               axis3 = trade,
+               y = tot_fed/1000000)) +
+  scale_x_discrete (limits = c ("nutrient", "fleet", "trade"), expand = c(.2, .05)) +
+  labs(y = "RNI equivalents, millions", x = "Allocation levers") +
+  geom_alluvium(aes(fill = nutrient)) +
+  geom_stratum() +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  theme_minimal() +
+  ggtitle("title",
+          "subtitle")
+
+
+
+# try peru
+# instead have middle axis be domestic, exported, foreign. assume all exports are fishmeal
+peru_exports <- sau_2019 %>%
+  filter (country == "Peru", fishing_entity == "Peru") %>%
+  left_join (exports_5yr_mean, by = c("species", "country")) %>%
+  replace_na (list(mn_prop_exp = 0)) %>%
+  mutate (Exports = tonnes * mn_prop_exp,
+          Domestic_catch = tonnes * (1-mn_prop_exp)) %>%
+  ungroup() %>%
+  select (species, Exports, Domestic_catch) %>%
+  pivot_longer (-species,
+                names_to = "Allocation", 
+                values_to = "tonnes") 
+
+peru_allocation <- sau_2019 %>%
+  # first take foreign fishing
+  filter (country == "Peru", fishing_entity != "Peru") %>%
+  mutate (Allocation = "Foreign_catch") %>%
+  group_by (species, Allocation) %>%
+  summarise (tonnes = sum (tonnes)) %>%
+  # just rbind to domestic/exported
+  rbind (peru_exports)
+
+# new df for end use
+# assume exports are in same proportion of end_use_type, even though we don't know
+# assume all exports are from domestic
+# make separate foreign and domestic
+
+# have to hack
+
+# mutate hack, fix peru anchovy. multiply total domestic anchov production * proportion non dhc from 2018
+
+  
+peru_end_use_dom <- sau_2019 %>%
+  filter (country == "Peru", fishing_entity == "Peru") %>%
+  group_by (country, species, end_use_type) %>%
+  summarise (tonnes = sum (tonnes)) %>%
+  mutate (tonnes = case_when (
+    species == "Engraulis ringens" & end_use_type == "Fishmeal and fish oil" ~ peru_anchov_dhc$prop_non_dhc * peru_anchov_total_2019,
+    species == "Engraulis ringens" & end_use_type == "Direct human consumption" ~ (1 - peru_anchov_dhc$prop_non_dhc) * peru_anchov_total_2019,
+    TRUE ~ tonnes)) %>%
+  left_join (exports_5yr_mean, by = c("species", "country")) %>%
+  replace_na (list(mn_prop_exp = 0)) %>%
+  mutate (Domestic_catch = tonnes * (1-mn_prop_exp),
+          Exports = tonnes * mn_prop_exp, .keep = "unused") %>%
+  pivot_longer (cols = c("Domestic_catch", "Exports"),
+                names_to = "Allocation",
+                values_to = "tonnes")
+
+peru_end_use_foreign <- sau_2019 %>%
+  filter (country == "Peru", fishing_entity != "Peru") %>%
+  mutate (Allocation = "Foreign_catch") %>%
+  group_by (country, species, end_use_type, Allocation) %>%
+  summarise (tonnes = sum (tonnes))%>%
+  mutate (tonnes = case_when (
+    species == "Engraulis ringens" & end_use_type == "Fishmeal and fish oil" ~ peru_anchov_dhc$prop_non_dhc * peru_anchov_total_2019_foreign,
+    species == "Engraulis ringens" & end_use_type == "Direct human consumption" ~ (1 - peru_anchov_dhc$prop_non_dhc) * peru_anchov_total_2019_foreign,
+    TRUE ~ tonnes)) 
+
+peru_end_use <- rbind (peru_end_use_dom, peru_end_use_foreign) %>%
+  mutate(rni_equivalents = pmap (list (species = species, amount = tonnes, country_name = "Peru"), calc_children_fed_func)) %>%
+  unnest (cols = c(rni_equivalents))  %>%
+  group_by (end_use_type, Allocation, nutrient) %>%
+  summarise (tot_fed= sum (rni_equivalents, na.rm = TRUE))
+
+indo_trade$trade <- factor (indo_trade$trade, levels = c("Kept", "Exported"))
+
+png ("Figures/Sankey_peru.png", width = 10, height = 6, units = "in", res = 300)
+peru_end_use %>%
+  filter (!nutrient %in% c("Protein", "Selenium"), !end_use_type == "Other") %>%
+  ggplot (aes (axis1 = nutrient, 
+               axis2 = Allocation,
+               axis3 = end_use_type,
+               y = tot_fed/1000000)) +
+  scale_x_discrete (limits = c ("nutrient", "Allocation", "end_use_type"), expand = c(.2, .05)) +
+  labs(y = "RNI equivalents, millions", x = "Allocation levers") +
+  geom_alluvium(aes(fill = nutrient)) +
+  geom_stratum() +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  theme_minimal() +
+  ggtitle("Nutrient flows, Peru") +
+  theme (axis.text = element_text (size = 14),
+         axis.title = element_text (size = 16),
+         legend.position = "none")
+  
+dev.off()
+
+table (peru_end_use$end_use_type, peru_end_use$tot_fed)
+
+#################################################################################################
 # plot exports and foreign fishing----
 
 # assume foreign catch is separate from exports. that is, export proportions are for domestic catch
@@ -331,6 +487,7 @@ dev.off()
 
 # Plot end use types ----
 # NOTE: 2019 SAU reconstruction no longer has discards, for any year. Discards were present in my previous download.
+sau_2019_taxa <- readRDS("Data/SAU_2019_taxa.Rds")
 
 plot_end_use_levers_aggregate <- function (country_name, Selenium = FALSE) {
 c <- sau_2019 %>%
