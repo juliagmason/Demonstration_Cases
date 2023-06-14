8aedcef12f83a8a756f8c35cc2b82a1b5b8e4dc6
juliagmason <juliagmason@gmail.com>
2023-06-14 16:21:10 -0400

    top 3 species by volume figs for each country

--- Scripts/Plot_nutrient_content_dodged_bar.R
+++ Scripts/Plot_nutrient_content_dodged_bar.R
@@ -94,7 +94,7 @@ plot_colorful_spp_nutr_dodge_bar(indo_spp$species, Selenium = TRUE)
 # Sardinella species, and scomberomorous
 
 
-# Malawi top spp
+# Malawi top spp ----
 mal_names <- read.csv ("Data/Malawi_names.csv") %>%
   rename (comm_name = Common.name,
           species = Scientific.name) %>%
@@ -124,14 +124,14 @@ plot_colorful_spp_nutr_dodge_bar(mal_top_names$species, Selenium = FALSE) +
 )
 dev.off()
 
-# sierra leone, priority species
+# sierra leone ----
 
 #sl_pri <- priority_spp %>% filter (country == "Sierra Leone")
 sl_landings <- readRDS("Data/SLE_landings_IHH.Rds")
 
 # top landed species
 sl_top <- sl_landings %>%
-  group_by (species, sector) %>%
+  group_by (species) %>%
   summarise (tonnes = sum(catch_mt, na.rm = TRUE)) %>%
   ungroup() %>%
   slice_max (n=6, order_by = "tonnes") # this gets 3 identifiable spp? big drop after P. elongatus
@@ -147,6 +147,147 @@ print (
 )
 dev.off()
 
+# john wanted top industrial vs. artisanal?
+sl_top_sector <- sl_landings %>%
+  filter (year == 2017) %>%
+  #summarise (tonnes = sum(catch_mt, na.rm = TRUE)) %>%
+  #ungroup() %>%
+  group_by (sector) %>%
+  slice_max (catch_mt, n=4)
+
+# ss is E. fim, Sard, P. elongatus.. Large is sard, c. rhoncus, G. decadactylus for 2017. same for overall years. 
+
+# plot with facet
+sl_facet <- spp_nutr %>%
+  filter (!nutrient %in% c("Selenium", "Protein"), 
+          species %in% sl_top_sector$species) %>%
+  group_by (species) %>%
+  mutate (micronutrient_density = sum (perc_rni),
+          spp_short = ifelse (
+            grepl(" ", species),
+            paste0 (substr(species, 1, 1), ". ", str_split_fixed (species, " ", 2)[,2]),
+            species)
+  ) %>% 
+  ungroup() %>%
+  mutate (sector = case_when (
+    species %in% c("Sardinella", "Ethmalosa fimbriata", "Pseudotolithus elongatus") ~ "Small-scale",
+    TRUE ~ "Large-scale"
+  ))
+
+ # make fake additional sardinella
+ls_sard <- sl_facet %>% filter (species == "Sardinella") %>% mutate (sector = "Large-scale")
+
+sl_facet <- rbind (sl_facet, ls_sard)
+sl_facet$sector <- factor (sl_facet$sector, levels = c ("Small-scale", "Large-scale"))
+
+
+png ("Figures/SL_spp_nutr_dodge_bar_sector.png", width = 6, height = 4, units = "in", res = 300)
+ sl_facet %>% 
+  ggplot (aes (x = reorder(spp_short, -micronutrient_density), fill = nutrient, y = perc_rni)) +
+  geom_col (position = "dodge") +
+  theme_bw() +
+  labs (x = "", y = "% Child RNI met per 100g serving", fill = "Nutrient") +
+  facet_wrap (~sector, scales = "free_x") +
+  #ylim (c(0,100)) +
+  ggtitle ("Nutrient content of top species by volume") +
+  theme ( 
+    axis.text.y = element_text (size = 13),
+    axis.text.x = element_text (size = 11, angle = 45, vjust =1, hjust = 1),
+    axis.title = element_text (size = 13),
+    strip.text = element_text(size = 13),
+    legend.text = element_text (size = 12),
+    legend.title = element_text (size = 13),
+    plot.title = element_text (size = 16))
+dev.off()
+
+
+# indonesia top spp ----
+indo_top <- sau_2019 %>%
+  filter (country == "Indonesia") %>%
+  group_by (species) %>%
+  summarise (tot = sum (tonnes)) %>%
+  slice_max (tot, n = 5)
+
+png ("Figures/Indo_top_spp_nutr_dodge_bar.png", width = 5, height = 4, units = "in", res = 300)
+print(
+  plot_colorful_spp_nutr_dodge_bar(indo_top$species, Selenium = FALSE) +
+    ggtitle ("Nutrient content of top species by volume") +
+    # scale_x_discrete(labels = c ())
+    theme ( 
+      axis.text.y = element_text (size = 13),
+      axis.text.x = element_text (size = 11),
+      axis.title = element_text (size = 13),
+      legend.text = element_text (size = 12),
+      legend.title = element_text (size = 13),
+      plot.title = element_text (size = 16))
+)
+dev.off()
+
+# peru spp ----
+peru_top <- sau_2019 %>%
+  filter (country == "Peru") %>%
+  group_by (species) %>%
+  summarise (tot = sum (tonnes)) %>%
+  slice_max (tot, n = 5)
+
+png ("Figures/Peru_top_spp_nutr_dodge_bar.png", width = 5, height = 4, units = "in", res = 300)
+print(
+  plot_colorful_spp_nutr_dodge_bar(peru_top$species, Selenium = FALSE) +
+    ggtitle ("Nutrient content of top species by volume") +
+    # scale_x_discrete(labels = c ())
+    theme ( 
+      axis.text.y = element_text (size = 13),
+      axis.text.x = element_text (size = 11),
+      axis.title = element_text (size = 13),
+      legend.text = element_text (size = 12),
+      legend.title = element_text (size = 13),
+      plot.title = element_text (size = 16))
+)
+dev.off()
+
+# chile ----
+chl_top <- chl_landings %>%
+  group_by (species) %>%
+  summarise (tot = sum (catch_mt)) %>%
+  slice_max (tot, n = 3)
+
+# algae is crazy
+chl_top_alg <- chl_landings %>%
+  filter (commercial_group == "Algae") %>%
+  group_by (species) %>%
+  summarise (tot = sum (catch_mt)) %>%
+  slice_max (tot, n = 3)
+
+png ("Figures/Chl_top_spp_nutr_dodge_bar_alg.png", width = 5, height = 4, units = "in", res = 300)
+print(
+  plot_colorful_spp_nutr_dodge_bar(chl_top_alg$species, Selenium = FALSE) +
+    ggtitle ("Nutrient content of top algal species by volume") +
+    # scale_x_discrete(labels = c ())
+    theme ( 
+      axis.text.y = element_text (size = 13),
+      axis.text.x = element_text (size = 11),
+      axis.title = element_text (size = 13),
+      legend.text = element_text (size = 12),
+      legend.title = element_text (size = 13),
+      plot.title = element_text (size = 16))
+)
+dev.off()
+
+
+png ("Figures/Chl_top_spp_nutr_dodge_bar_non-alg.png", width = 5, height = 4, units = "in", res = 300)
+print(
+  plot_colorful_spp_nutr_dodge_bar(chl_top$species, Selenium = FALSE) +
+    ggtitle ("Nutrient content of top species by volume") +
+    # scale_x_discrete(labels = c ())
+    theme ( 
+      axis.text.y = element_text (size = 13),
+      axis.text.x = element_text (size = 11),
+      axis.title = element_text (size = 13),
+      legend.text = element_text (size = 12),
+      legend.title = element_text (size = 13),
+      plot.title = element_text (size = 16))
+)
+dev.off()
 
 ##### Priority species plots ----
 # quick peru plot
