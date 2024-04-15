4dfc82ab36083d974d7f7ac8bb6d8b467a0a4047
juliagmason <juliagmason@gmail.com>
2024-04-15 11:19:42 -0400

    cleaning/streamlining multiple plots

--- Scripts/Plot_Figures/Plot_Fig_XE_nutrient_projections.R
+++ Scripts/Plot_Figures/Plot_Fig_XE_nutrient_projections.R
@@ -4,7 +4,7 @@
 
 # plot annual timeseries of projected nutrient yield, in child RNI equivalents
 # calculated these in Calculate_projected_nutritional_upsides.R
-# note--Peru rds does not include anchoveta. Peru_anchoveta is anchoveta only. treat these as different country names
+# note--Peru and Chile rds does not include anchoveta. Peru_anchoveta is anchoveta only. treat these as different country names
 
 library (tidyverse)
 # plot annual ts-----
@@ -21,12 +21,23 @@ plot_child_RNI_proj <- function (country_name, RCP) {
     summarise (tot_fed = sum (rni_equivalents, na.rm = TRUE)) %>%
     filter (rcp == RCP, !nutrient %in% c("Protein"), scenario %in% c("Full Adaptation", "No Adaptation"))
   
+  # for supplement only
+  # # specify anchoveta in title
+  # # https://stackoverflow.com/questions/40113963/how-to-extract-everything-until-first-occurrence-of-pattern
+  # if (country_name %in% c("Peru_anchoveta", "Chile_anchoveta")) {
+  #   country_title <-  gsub("(.+?)(\\_.*)", "\\1", country_name)
+  #   plot_title <- paste0("Projected nutrient yield for ", country_title, "; ", RCP, "; Anchoveta only")
+  # } else if (country_name %in% c("Peru", "Chile")) {
+  #   plot_title <- paste0("Projected nutrient yield for ", country_name, "; ", RCP, "; Anchoveta removed")
+  #   } else {plot_title <- paste0("Projected nutrient yield for ", country_name, "; ", RCP)}
+  # 
   plot_ts <- nutr_agg_ts %>% 
     ggplot (aes (x = year, y = tot_fed/1000000, col = scenario, group = scenario)) +
     geom_line() +
     facet_wrap (~nutrient, scales = "free_y") +
     theme_bw() +
     labs (y = "Child RNI equivalents, millions", x = "", col = "Mgmt\nscenario") +
+    #ggtitle (plot_title) +
     ggtitle (paste0("Projected nutrient yield for ", country_name, "; ", RCP)) +
     theme (axis.text.y = element_text (size = 14),
            axis.text.x = element_text (size = 13),
@@ -38,112 +49,43 @@ plot_child_RNI_proj <- function (country_name, RCP) {
   
   plot_ts 
   
-  # # save png - use this for supplemental figs
+  # # save png - use this for supplemental figs only
   # png (paste0("Figures/annual_nutr_ts_childRNI_", country_name, "_", RCP, ".png"), width = 9, height = 6, units = "in", res = 300)
   # print (plot_ts + theme(legend.position="bottom"))
   # dev.off()
   
 }
 
-plot_child_RNI_proj("Chile", RCP = "RCP26")
-plot_child_RNI_proj("Peru_anchoveta", RCP = "RCP60")
+# plot_child_RNI_proj("Chile", RCP = "RCP26")
+# plot_child_RNI_proj("Peru_anchoveta", RCP = "RCP60")
+countries <- c("Peru", "Peru_anchoveta", "Chile", "Chile_anchoveta", "Indonesia", "Sierra Leone")
 
-plot_child_RNI_proj("Indonesia", RCP = "RCP60") +
-  labs (y = "Child RNI equiv., millions", x = "", col = "Mgmt. scenario") +
-  ggtitle ("Projected nutrient yield, RCP 6.0") +
-  scale_x_continuous (breaks = c(2020, 2050, 2090)) +
-  theme ( 
-    axis.text.x = element_text (size = 9),
-    axis.text.y = element_text (size = 9),
-    axis.title = element_text (size = 12),
-    legend.text = element_text (size = 9),
-    legend.title = element_text (size = 9),
-    legend.position = "bottom",
-    legend.margin = margin (t = -25),
-    strip.text = element_text (size = 9, margin = margin(0.5,0.5,0.5,0.5, "mm")),
-    plot.title = element_text (size = 13),
-    plot.margin=unit(c(1,1,1,1), 'mm'),
-    panel.grid.minor = element_blank())
-
-ggsave ("Figures/FigXE_projected_Indo.eps", width = 97, height = 70, units = "mm")
-
-plot_child_RNI_proj("Chile", RCP = "RCP60") +
-  labs (y = "Child RNI equiv., millions", x = "", col = "Mgmt. scenario") +
-  ggtitle ("Projected nutrient yield, RCP 6.0") +
-  scale_x_continuous (breaks = c(2020, 2050, 2090)) +
-  theme ( 
-    axis.text.x = element_text (size = 9),
-    axis.text.y = element_text (size = 9),
-    axis.title = element_text (size = 12),
-    legend.text = element_text (size = 9),
-    legend.title = element_text (size = 9),
-    legend.position = "bottom",
-    legend.margin = margin (t = -25),
-    strip.text = element_text (size = 9, margin = margin(0.5,0.5,0.5,0.5, "mm")),
-    plot.title = element_text (size = 13),
-    plot.margin=unit(c(1,1,1,1), 'mm'),
-    panel.grid.minor = element_blank())
-
-ggsave ("Figures/FigXE_projected_Chile.eps", width = 97, height = 70, units = "mm")
-
-plot_child_RNI_proj("Sierra Leone", RCP = "RCP60") +
-  labs (y = "Child RNI equiv., millions", x = "", col = "Mgmt. scenario") +
-  ggtitle ("Projected nutrient yield, RCP 6.0") +
-  scale_x_continuous (breaks = c(2020, 2050, 2090)) +
-  theme ( 
-    axis.text.x = element_text (size = 9),
-    axis.text.y = element_text (size = 9),
-    axis.title = element_text (size = 12),
-    legend.text = element_text (size = 9),
-    legend.title = element_text (size = 9),
-    legend.position = "bottom",
-    legend.margin = margin (t = -25),
-    strip.text = element_text (size = 9, margin = margin(0.5,0.5,0.5,0.5, "mm")),
-    plot.title = element_text (size = 13),
-    plot.margin=unit(c(1,1,1,1), 'mm'),
-    panel.grid.minor = element_blank())
-
-ggsave ("Figures/FigXE_projected_SierraLeone.eps", width = 97, height = 70, units = "mm")
+for (country in countries) {
+  
+  plot_child_RNI_proj(country, RCP = "RCP60") +
+    labs (y = "Child RNI equiv., millions", x = "", col = "Mgmt. scenario") +
+    ggtitle ("Projected nutrient yield, RCP 6.0") +
+    scale_x_continuous (breaks = c(2020, 2040, 2060), limits = c(2020, 2060)) +
+    theme ( 
+      axis.text.x = element_text (size = 9),
+      axis.text.y = element_text (size = 9),
+      axis.title = element_text (size = 12),
+      legend.text = element_text (size = 9),
+      legend.title = element_text (size = 9),
+      legend.position = "bottom",
+      legend.margin = margin (t = -25),
+      strip.text = element_text (size = 9, margin = margin(0.5,0.5,0.5,0.5, "mm")),
+      plot.title = element_text (size = 13),
+      plot.margin=unit(c(1,1,1,1), 'mm'),
+      panel.grid.minor = element_blank())
+  
+  ggsave (paste0("Figures/FigXE_projected_", country, ".eps", width = 97, height = 70, units = "mm"))
+  
+}
 
 
-plot_child_RNI_proj("Peru", RCP = "RCP60") +
-  labs (y = "Child RNI equiv., millions", x = "", col = "Mgmt. scenario") +
-  ggtitle ("Projected nutrient yield, RCP 6.0, Anchoveta excluded") +
-  scale_x_continuous (breaks = c(2020, 2050, 2090)) +
-  theme ( 
-    axis.text.x = element_text (size = 9),
-    axis.text.y = element_text (size = 9),
-    axis.title = element_text (size = 12),
-    legend.text = element_text (size = 9),
-    legend.title = element_text (size = 9),
-    legend.position = "bottom",
-    legend.margin = margin (t = -25),
-    strip.text = element_text (size = 9, margin = margin(0.5,0.5,0.5,0.5, "mm")),
-    plot.title = element_text (size = 13),
-    plot.margin=unit(c(1,1,1,1), 'mm'),
-    panel.grid.minor = element_blank())
-
-ggsave ("Figures/FigXE_projected_Peru_no_anchoveta.eps", width = 97, height = 70, units = "mm")
-
-plot_child_RNI_proj("Peru_anchoveta", RCP = "RCP60") +
-  labs (y = "Child RNI equiv., millions", x = "", col = "Mgmt. scenario") +
-  ggtitle ("Projected nutrient yield, RCP 6.0, Anchoveta only") +
-  scale_x_continuous (breaks = c(2020, 2050, 2090)) +
-  theme ( 
-    axis.text.x = element_text (size = 9),
-    axis.text.y = element_text (size = 9),
-    axis.title = element_text (size = 12),
-    legend.text = element_text (size = 9),
-    legend.title = element_text (size = 9),
-    legend.position = "bottom",
-    legend.margin = margin (t = -25),
-    strip.text = element_text (size = 9, margin = margin(0.5,0.5,0.5,0.5, "mm")),
-    plot.title = element_text (size = 13),
-    plot.margin=unit(c(1,1,1,1), 'mm'),
-    panel.grid.minor = element_blank())
-
-ggsave ("Figures/FigXE_projected_Peru_anchoveta.eps", width = 97, height = 70, units = "mm")
-
-
-# Apply to all countries, all RCPs for supplement
-pmap (expand_grid(country_name = c ("Chile", "Peru", "Sierra Leone", "Indonesia"), RCP = c("RCP26", "RCP45", "RCP60", "RCP85")), plot_child_RNI_proj)                   
+# # Apply to all countries, all RCPs for supplement
+# pmap (expand_grid(country_name = c ("Chile", "Peru", "Sierra Leone", "Indonesia"), RCP = c("RCP26", "RCP45", "RCP60", "RCP85")), plot_child_RNI_proj)                   
+# 
+# # just chile redo
+# pmap (expand_grid(country_name = c ("Chile", "Chile_anchoveta"), RCP = c("RCP26", "RCP45", "RCP60", "RCP85")), plot_child_RNI_proj)
