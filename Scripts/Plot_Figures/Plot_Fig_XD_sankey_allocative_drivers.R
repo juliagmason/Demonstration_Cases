cc9193ce2289a85654b5096e0e3c0bc3ddab5ff4
juliagmason <juliagmason@gmail.com>
2024-04-17 18:31:07 -0400

    chile no anchoveta, and adding selenium back in

--- Scripts/Plot_Figures/Plot_Fig_XD_sankey_allocative_drivers.R
+++ Scripts/Plot_Figures/Plot_Fig_XD_sankey_allocative_drivers.R
@@ -10,13 +10,13 @@ library (ggalluvial)
 
 # going back to just exports for all countries ----
 plot_exports_sankey <- function (country_name) {
-  # country names are Chile, Peru, Peru_anchov, Peru_noanchov, SierraLeone, Indo 
+  # country names are Chile *this has anchov excluded, Chle_anchov, Peru, Peru_anchov, Peru_noanchov, SierraLeone, Indo 
  
   export_nutr <- readRDS(paste0("Data/levers_RNI_pop_export_", country_name, ".Rds"))
   # these were calculated in calculate_national_allocative_levers.R
   
   export_nutr  %>% 
-    filter (!nutrient %in% c("Protein", "Selenium")) %>%
+    filter (!nutrient %in% c("Protein")) %>%
     mutate (nutrient = case_when (
       nutrient == "Omega_3" ~ "Omega 3",
       nutrient == "Vitamin_A" ~ "Vit. A",
@@ -32,7 +32,8 @@ plot_exports_sankey <- function (country_name) {
     geom_text(stat = "stratum", aes(label = after_stat(stratum)), size = 2.5) +
     theme_minimal() +
     ggtitle("National allocative drivers") +
-    scale_fill_brewer (palette = "Set1") +
+    #scale_fill_brewer (palette = "Set1") +
+    scale_fill_manual(values = c("#E41A1C", "#377EB8", "#4DAF4A","#FFFF33", "#984EA3", "#FF7F00"))
     theme ( 
       axis.text.x = element_blank(),
       axis.text.y = element_text (size = 9),
@@ -44,5 +45,4 @@ plot_exports_sankey <- function (country_name) {
   ggsave (paste0("Figures/FigXD_exports_driver_", country_name, ".svg"), width = 74, height = 70, units = "mm")
 }
 
-plot_exports_sankey("SierraLeone")
-
+map (c("Chile", "Chile_anchov", "Peru_anchov", "Peru_noanchov", "SierraLeone", "Indo"), plot_exports_sankey)
