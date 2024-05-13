c5af08b547dba7bcd1250fee0d427d922e40edba
juliagmason <juliagmason@gmail.com>
2024-05-13 13:17:31 -0400

    Faceted plots with all nutrients and RCPs

--- Scripts/Plot_Figures/Plot_supplemental_upsides_pop_demand.R
+++ Scripts/Plot_Figures/Plot_supplemental_upsides_pop_demand.R
@@ -3,16 +3,115 @@
 # 4/15/24
 #moving from Calculate_population_percent_nutrition...R
 
+library (tidyverse)
 
 # plot population requirements met time series ----
 
 # These will go in supplement. Plot for all RCPs
 
-# this takes the forecasted nutrients in tonnes from the above chunk of code and relates to populatio nnutrient needs
+# this takes the forecasted nutrients in tonnes from the above chunk of code and relates to population nutrient needs
 
-# if running from here:
+# population growth
 wpp_country_aggregate <- readRDS("Data/annual_nutr_demand_rni_by_country.Rds")
 
+# facet by RCP and nutrient for fewer figures
+
+plot_pop_needs_proj_facet <- function (country_name, anchovy = FALSE) {
+  
+  tonnes_nutr_ts <- readRDS(paste0("Data/", country_name, "_annual_ts_forecasted_nutrients_tonnes.Rds"))
+  
+  if (country_name %in% c("Chile", "Peru") & anchovy == TRUE) {
+    tonnes_nutr_agg_ts <-  tonnes_nutr_ts %>%
+      filter (species == "Engraulis ringens") %>%
+      group_by (year, nutrient, rcp, scenario) %>%
+      summarise (tot_tonnes = sum (nutr_tonnes, na.rm = TRUE))
+    
+    plot_title <- paste0("Projected nutrient yield for ", strsplit (country_name, "_")[[1]][1], "; Anchoveta only")
+    
+    plot_filename <- paste0("Figures/annual_nutr_ts_population_facet_", country_name, "_anchov.png")
+    
+  } else if (country_name %in% c("Chile", "Peru") & anchovy == FALSE) {
+    
+    tonnes_nutr_agg_ts <-  tonnes_nutr_ts %>%
+      filter (species != "Engraulis ringens") %>%
+      group_by (year, nutrient, rcp, scenario) %>%
+      summarise (tot_tonnes = sum (nutr_tonnes, na.rm = TRUE))
+    
+    plot_title <- paste0("Projected nutrient yield for ", country_name, "; Anchoveta removed")
+    
+    plot_filename <- paste0("Figures/annual_nutr_ts_population_facet_", country_name, "_noanchov.png")
+    
+  } else {
+    
+    tonnes_nutr_agg_ts <-  tonnes_nutr_ts %>%
+      group_by (year, nutrient, rcp, scenario) %>%
+      summarise (tot_tonnes = sum (nutr_tonnes, na.rm = TRUE))
+    
+    plot_title <- paste0("Projected nutrient yield for ", country_name)
+    
+    plot_filename <- paste0("Figures/annual_nutr_ts_population_facet_", country_name, ".png")
+  }
+  
+  # aggregate by nutrient, rcp, scenario
+  
+  
+  # join to population
+  ts_perc_pop <- wpp_country_aggregate %>%
+    filter (country == country_name, Time > 2022) %>%
+    rename (year = Time) %>%
+    left_join (tonnes_nutr_agg_ts, by = c ("year", "nutrient")) %>%
+    mutate (prop_demand_met = tot_tonnes / tot_nutr_annual_demand)
+  
+  # fix levels
+  ts_perc_pop$scenario  <- factor(ts_perc_pop$scenario, levels = c ("No Adaptation", "Productivity Only", "Full Adaptation"))
+  
+  # plot
+  plot <- ts_perc_pop %>%
+    filter (!nutrient %in% c("Protein"), !is.na (rcp)) %>%
+    mutate (nutrient = gsub ("_", " ", nutrient),
+            # https://stackoverflow.com/questions/13863599/insert-a-character-at-a-specific-location-in-a-string
+            # clumsy but here we go
+            # insert decimal between the digits
+            rcp = gsub('^(.{4})(.*)$', '\\1.\\2', rcp),
+            # insert space after the letters
+            rcp = gsub('^(.{3})(.*)$', '\\1 \\2', rcp)) %>%
+    ggplot (aes (x = year, y = prop_demand_met * 100, col = scenario, group = scenario)) +
+    #geom_point() +
+    geom_line() +
+    facet_wrap (nutrient~rcp, scales = "free_y", ncol = 4) +
+    theme_bw() +
+    labs (y = "% population RNI equiv.", x = "", col = "Mgmt. scenario") +
+    ggtitle (plot_title) +
+    theme (axis.text.y = element_text (size = 12),
+           axis.text.x = element_text (size = 10),
+           axis.title = element_text (size = 14),
+           strip.text = element_text (size = 10),
+           legend.text = element_text (size = 12),
+           legend.title = element_text (size = 14),
+           plot.title = element_text (size = 18),
+           # bring legend closer to plot
+           # legend.margin=margin(1,1,1,1),
+           # legend.box.margin=margin(-10,-10,-10,0),
+           legend.box.spacing = margin(0.2),
+           legend.position = "bottom",
+           plot.margin=unit(c(1,1,1,1), 'mm'))
+  
+  #return (plot)
+  
+  # save png
+  png (plot_filename, width = 7.5, height = 9.5, units = "in", res = 300)
+  print(plot)
+  dev.off()
+  
+}
+
+country_names <- c("Peru", "Chile", "Sierra Leone", "Indonesia")
+
+map (country_names, plot_pop_needs_proj_facet)
+map (c("Chile", "Peru"), plot_pop_needs_proj_facet, anchovy = TRUE)
+
+
+# One RCP focal plots ----
 plot_pop_needs_met_proj <- function (country_name, RCP) {
   
   tonnes_nutr_ts <- readRDS(paste0("Data/", country_name, "_annual_ts_forecasted_nutrients_tonnes.Rds"))
